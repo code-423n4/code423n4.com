f11839f124304bc093654e009f8f357275d61d17
Adam Brault <adam@andyet.net>
2021-03-03 13:33:11 -0800

    add orgs schema

---
+++ schema/OrgsJson.js
@@ -0,0 +1,11 @@
+const OrgsJson = `
+type OrgsJson implements Node {
+  name:           String
+  link:           String
+  image:          String
+  description:    String
+  contests:       [ ContestsJson ] @link(by: "id", from: "contests")
+  findings:       [ FindingsJson ] @link(by: "id", from: "findings")
+}`
+
+export default OrgsJson
