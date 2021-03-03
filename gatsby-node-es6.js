9f9bca7ecfbde0988b7a3fc359a60fe0a1b544cf
Adam Brault <adam@andyet.net>
2021-03-03 13:28:52 -0800

    add es6 shim and initial gatsby node file

---
+++ gatsby-node-es6.js
@@ -0,0 +1,8 @@
+import path from "path"
+import SchemaCustomization from "./schema"
+import GraphQLQueries from "./queries"
+
+// const _ = require("lodash")
+// const remark = require("remark")
+// const remarkHTML = require("remark-html")
+// const truncate = require("truncate-html")
