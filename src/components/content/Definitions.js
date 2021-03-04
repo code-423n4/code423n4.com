3f3838707bd99c000076c8d168d494d5805e37bd
Adam Brault <adam@andyet.net>
2021-03-04 08:09:07 -0800

    tweak structure

---
+++ src/components/content/Definitions.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Definitions = () => {
+  return (
+    <div className="definitions">
+      <div>
+        <h3>Wardens</h3>
+        <p>Hunt exploits in the smart contracts of decentralized protocols.</p>
+      </div>
+      <div>
+        <h3>Sponsors</h3>
+        <p>Create bounty pools to attract wardens to audit your project.</p>
+      </div>
+      <div>
+        <h3>Judges</h3>
+        <p>Allocate bounty pools to wardens based on performance.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Definitions;
