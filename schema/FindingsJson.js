9dbaa876e2550647cf38fb05d7c53057bbcc087f
Adam Avenir <adam@andyet.com>
2021-03-05 09:26:38 -0800

    tweak findings data structure

--- schema/FindingsJson.js
+++ schema/FindingsJson.js
@@ -1,9 +1,13 @@
 const FindingsJson = `
 type FindingsJson implements Node {
+  id:            String
   contest:       ContestJson @link(by: "id", from: "contest")
   recipient:     [ PeopleJson ] @link(by: "id", from: "people")
+  title:         String
+  description:   String
+  severity:      String
   type:          String
   award:         Int
-}`
+}`;
 
-export default FindingsJson
+export default FindingsJson;
