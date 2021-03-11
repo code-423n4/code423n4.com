731e9a4838e3424d716cf04482e1e6f98343a87a
Adam Brault <adam@andyet.net>
2021-03-11 13:57:42 -0800

    update contest components

--- src/components/Contest.js
+++ src/components/Contest.js
@@ -2,23 +2,45 @@ import React from "react";
 import Countdown from "./Countdown";
 
 const Contest = ({ contest }) => {
-  const { sponsor, details, end_time } = contest;
+  const {
+    sponsor,
+    title,
+    details,
+    start_time,
+    end_time,
+    wardens,
+    judges,
+  } = contest;
+
+  const now = new Date().getTime();
+  const start = new Date(start_time).getTime();
+  const end = new Date(end_time).getTime();
+
+  let state;
+  if (now < start) {
+    state = "soon";
+  }
+  if (now >= start && now <= end) {
+    state = "active";
+  }
+  if (now > end) {
+    state = "completed";
+  }
 
   return (
-    <div className="wrapper-contest">
+    <div className={"wrapper-contest " + state}>
       <div className="wrapper-sponsor">
-        <a className="sponsor-logo" href={sponsor.link}>
+        <a className="sponsor-logo" href={sponsor.image}>
           <img src={sponsor.image} alt={sponsor.name} />
         </a>
       </div>
       <div className="wrapper-contest-content">
-        <h4>
-          <a className="sponsor-name" href={sponsor.link}>
-            {sponsor.name}
-          </a>{" "}
-          contest
-        </h4>
-        <Countdown deadline={end_time} />
+        <h4>{title}</h4>
+        {state === "soon" || state === "active" ? (
+          <Countdown state={state} start={start_time} end={end_time} />
+        ) : (
+          "ended"
+        )}
         <p>{details}</p>
       </div>
     </div>
