f4a84dc48045158488123e72d0e23abc0bd092e9
Adam Brault <adam@andyet.net>
2021-03-10 21:48:48 -0800

    add schemacustomization

--- gatsby-node-es6.js
+++ gatsby-node-es6.js
@@ -1,8 +1,18 @@
-import path from "path"
-import SchemaCustomization from "./schema"
-import GraphQLQueries from "./queries"
+import path from "path";
+import SchemaCustomization from "./schema";
+import GraphQLQueries from "./queries";
 
 // const _ = require("lodash")
 // const remark = require("remark")
 // const remarkHTML = require("remark-html")
 // const truncate = require("truncate-html")
+
+exports.createSchemaCustomization = (helpers) => {
+  const { actions } = helpers;
+  const { createTypes } = actions;
+  try {
+    createTypes(SchemaCustomization);
+  } catch (error) {
+    console.log(error);
+  }
+};
