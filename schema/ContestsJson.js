3cb8d861bae4c72526d2d586b7f1286f3889cb08
Adam Brault <adam@andyet.net>
2021-03-11 13:56:58 -0800

    update contest data model

--- schema/ContestsJson.js
+++ schema/ContestsJson.js
@@ -4,12 +4,17 @@ type ContestsJson implements Node {
   sponsor:        OrgsJson @link(by: "name", from: "sponsor")
   title:          String
   details:        String
-  active:         Boolean  
   start_time:     Date
   end_time:       Date  
   wardens:        [ PeopleJson ] @link(by: "name", from: "people")
   judges:         [ PeopleJson ] @link(by: "name", from: "people")
   findings:       [ FindingsJson ] @link(by: "id", from: "findings")
+  hide:           Boolean  
 }`;
 
 export default ContestsJson;
+
+// contests: allContestsJson(
+//   filter: { hide: { ne: true } }
+//   sort: { fields: end_time, order: ASC }
+// )
