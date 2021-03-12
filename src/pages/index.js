d66a0827b834e971138bebc286192ac2c26428fe
Adam Avenir <adam@andyet.com>
2021-03-11 16:11:00 -0800

    add contest list page, update to gatsby 3 approach to es6

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,41 +4,35 @@ import DefaultLayout from "../layouts/DefaultLayout";
 import HeroIndex from "../components/content/HeroIndex";
 import Definitions from "../components/content/Definitions";
 import ContestList from "../components/ContestList";
-// import CONTESTS from "../../queries/CONTESTS";
 
-class SiteIndex extends React.Component {
-  render() {
-    const { data } = this.props;
-    const contests = data.contests.edges;
+export default function SiteIndex({ data }) {
+  const contests = data.contests.edges;
 
-    return (
-      <DefaultLayout title="Code 423n4" bodyClass="landing">
-        <HeroIndex />
-        <div className="wrapper-main">
-          <section>
-            <Definitions />
-            {contests ? <ContestList contests={contests} /> : ""}
-          </section>
-          <section>
-            <h5>Want to learn more?</h5>
-            <div className="button-wrapper">
-              <a
-                className="button cta-button"
-                href="https://medium.com/@scott_lew_is/introducing-code-432n4-f4a12d92a35d"
-              >
-                <strong>Read the intro post</strong>
-              </a>
-            </div>
-          </section>
-        </div>
-      </DefaultLayout>
-    );
-  }
+  return (
+    <DefaultLayout title="Code 423n4" bodyClass="landing">
+      <HeroIndex />
+      <div className="wrapper-main">
+        <section>
+          <Definitions />
+          {contests ? <ContestList contests={contests} /> : ""}
+        </section>
+        <section>
+          <h5>Want to learn more?</h5>
+          <div className="button-wrapper">
+            <a
+              className="button cta-button"
+              href="https://medium.com/@scott_lew_is/introducing-code-432n4-f4a12d92a35d"
+            >
+              <strong>Read the intro post</strong>
+            </a>
+          </div>
+        </section>
+      </div>
+    </DefaultLayout>
+  );
 }
 
-export default SiteIndex;
-
-export const pageQuery = graphql`
+export const query = graphql`
   query {
     contests: allContestsJson(
       filter: { hide: { ne: true } }
