bd9c0a9a4fb40b0bec5d84c9fb6b529135a2048d
Adam Brault <adam@andyet.net>
2021-03-29 09:11:20 -0700

    cleanup

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -1,11 +1,5 @@
 import path from "path";
 import SchemaCustomization from "./schema";
-// import GraphQLQueries from "./queries";
-
-// const _ = require("lodash")
-// const remark = require("remark")
-// const remarkHTML = require("remark-html")
-// const truncate = require("truncate-html")
 
 exports.createSchemaCustomization = (helpers) => {
   const { actions } = helpers;
