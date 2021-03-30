bd902fe59038b556b3ee65cfe7dff70f4747d138
Adam Brault <adam@andyet.net>
2021-03-30 12:04:57 -0700

    update contests

--- src/components/content/Footer.js
+++ src/components/content/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
       <a href="https://twitter.com/code423n4">Twitter</a>
       <a href="https://discord.gg/EY5dvm3evD">Discord</a>
       <a href="https://github.com/code-423n4/">GitHub</a>
+      <span class="eth-address">0xC2bc2F890067C511215f9463a064221577a53E10</span>
     </footer>
   );
 };
