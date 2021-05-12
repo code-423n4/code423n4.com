738dd0c384842bc0d354799ca3a32617fa1059a6
Dylan Staley <88163+dstaley@users.noreply.github.com>
2021-05-12 14:23:07 -0700

    let gatsby infer image field type

--- schema/HandlesJson.js
+++ schema/HandlesJson.js
@@ -3,7 +3,6 @@ type HandlesJson implements Node {
   name:           String
   handle:         String
   link:           String
-  image:          File
   contests:       [ ContestsJson ] @link(by: "id", from: "contests")
   findings:       [ FindingsJson ] @link(by: "id", from: "findings")
   members:        [ HandlesJson ] @link(by: "handle", from: "members")
