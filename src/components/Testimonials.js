57a78ff0c6d14e5fae30357faf8bf5e443f3fb79
Diana <diana@dianaperkinsdesign.com>
2021-08-09 17:29:39 -0400

    Adding sponsor and warden testimonials

---
+++ src/components/Testimonials.js
@@ -0,0 +1,123 @@
+import React from "react";
+import TweetEmbed from "../components/TweetEmbed";
+
+
+const Testimonials = () => (
+  <>
+    <h5 className="center testimonials-header">The crowd goes wild</h5>
+    <div className="testimonials">
+      <TweetEmbed tweetId="1400466820759375876" />
+      <figure>
+        <blockquote>
+          <p>
+            This result far exceeded previous experiences I’ve had with 2–4
+            week-long audits by individual companies. I highly recommend the
+            Code 423n4 competitive audit approach for anyone starting a new
+            project and propose that we use it again in the future as we roll
+            out new smart contracts.
+          </p>
+        </blockquote>
+        <figcaption>
+          LSDan <cite>ElasticDAO</cite>
+        </figcaption>
+      </figure>
+      <figure>
+        <blockquote>
+          <p>Thanks guys, very happy with the collaboration.</p>
+        </blockquote>
+        <figcaption>
+          Sid Powell<cite>Maple</cite>
+        </figcaption>
+      </figure>
+      <TweetEmbed tweetId="1388402434720997378" />
+      <TweetEmbed tweetId="1382498579118313473" />
+      <TweetEmbed tweetId="1402705202239713285" />
+      <figure>
+        <blockquote>
+          <p>
+            [C4 is] a really cool way to upskill your auditing skills, and makes
+            for a cool hobby/side hustle.
+          </p>
+        </blockquote>
+        <figcaption>
+          ToastedSteakSandwich<cite>C4 Warden</cite>
+        </figcaption>
+      </figure>
+      <TweetEmbed tweetId="1398424011336527873" />
+      <TweetEmbed tweetId="1409890941264044034" />
+      <TweetEmbed tweetId="1404727395706355715" />
+      <figure>
+        <blockquote>
+          <p>
+            Overall, I have to say you guys have some very smart folks and some
+            of the findings identified are quite good! Definitely a much better
+            experience than the traditional audit we had gone through.
+          </p>
+        </blockquote>
+        <figcaption>
+          Jason<cite>Fairside</cite>
+        </figcaption>
+      </figure>
+      <TweetEmbed tweetId="1407460200316088321" />
+      <figure>
+        <blockquote>
+          <p>The wardens really did a number on what we've built!</p>
+        </blockquote>
+        <figcaption>
+          Brandon<cite>Fairside</cite>
+        </figcaption>
+      </figure>
+      <TweetEmbed tweetId="1400746871719710728" />
+      <TweetEmbed tweetId="1400747819259023360" />
+      <figure>
+        <blockquote>
+          <p>
+            Ability to review and learn about different projects of DeFi. C4 contests attract new, unique and complicated projects so it is a perfect learning opportunity compared to 2017 when most of the contracts were either ERC20 tokens or ICOs. Here projects seriously invest in their security so it is always a motivation to try my best to find bugs in their code.
+          </p>
+        </blockquote>
+        <figcaption>
+          Pauliax/Thunder<cite>C4 Warden</cite>
+        </figcaption>
+      </figure>
+      <TweetEmbed tweetId="1410702378886705152" />
+      <figure>
+        <blockquote>
+          <p>
+            Want to give a massive shout out to all the wardens that took part
+            in the Reality Cards contest last week- you guys found some really
+            amazing stuff- so good that we're considering doing a second round!
+            🤣thanks so much guys!
+          </p>
+        </blockquote>
+        <figcaption>
+          Andrew<cite>Reality Cards</cite>
+        </figcaption>
+      </figure>
+      <TweetEmbed tweetId="1411461169701867525" />
+      <figure>
+        <blockquote>
+          <p>
+            Great to see so many submissions... All the issues are looking
+            really good, we got a lot of great feedback.
+          </p>
+        </blockquote>
+        <figcaption>
+          Ray<cite>Tracer</cite>
+        </figcaption>
+      </figure>
+      <TweetEmbed tweetId="1413422082373918722" />
+      <figure>
+        <blockquote>
+          <p>
+            Keeping up to date with the latest products, C4 has quite decent
+            quality and diversity in its projects which really trains you in the
+            long run.
+          </p>
+        </blockquote>
+        <figcaption>Anonymous Warden</figcaption>
+      </figure>
+    </div>
+  </>
+);
+
+export default Testimonials;
