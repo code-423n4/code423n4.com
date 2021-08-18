052cb1297e3e18ce84c8f1d00d569b95d282e13b
Adam Avenir <adam@andyet.com>
2021-08-18 09:25:52 -0700

    Update DefaultLayout.js

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -66,7 +66,7 @@ const DefaultLayout = (props) => {
         <div class="message-bar">
           <p>
           ✨ New!✨ &nbsp;C4 Cosmos league. First contest $100k+ audit of Gravity Bridge.&nbsp;
-            <a href="https://medium.com/@sockdrawermoney/code4rena-comes-to-cosmos-29269f4d13d">Read more »</a>
+            <a href="https://medium.com/code-423n4/code4rena-comes-to-cosmos-29269f4d13d">Read more »</a>
           </p>
           </div>
         <Nav />
