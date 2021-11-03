fd020cbc10afb1d9301daf3a7f8f3fc1d5f9950f
Diana <diana@dianaperkinsdesign.com>
2021-11-03 18:50:07 -0400

    adding tabs, starting styling

--- src/layouts/ContestLayout.js
+++ src/layouts/ContestLayout.js
@@ -2,14 +2,16 @@ import React, { useState } from "react";
 import { graphql } from "gatsby";
 import DefaultLayout from "./DefaultLayout";
 import ReactMarkdown from "react-markdown";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const ContestLayout = (props) => {
   const [artOpen, setArtOpen] = useState(false);
   let artClass = artOpen ? "open" : null;
-  const { title, sponsor, details, fields } = props.data.contestsCsv;
+  const { title, sponsor, details, fields, amount } = props.data.contestsCsv;
   const artURL = "http://placeimg.com/1200/675/arch/grayscale";
   return (
-    <DefaultLayout pageTitle={title} bodyClass="contests-page">
+    <DefaultLayout pageTitle={title} bodyClass="contest-page">
       <article className="wrapper-main">
         <div
           style={{ backgroundImage: `url(${artURL})` }}
@@ -23,22 +25,53 @@ const ContestLayout = (props) => {
               <img
                 src={sponsor.image.childImageSharp.resize.src}
                 alt={sponsor.name}
+                width="90px"
+                height="90px"
               />
             </a>
           </div>
-          <h1 className="header-live">{title}</h1>
-          <p>{details}</p>
-          <div class="button-wrapper">
-            <a
-              href={fields.submissionPath}
-              className="button button-small primary"
-            >
-              Submit Finding
-            </a>
+          <div class="top-section-text">
+            <h1 className="header-live">{title}</h1>
+            <p>{details}</p>
+            <div class="button-wrapper">
+              <a
+                href={fields.submissionPath}
+                className="button button-small primary"
+              >
+                Submit Finding
+              </a>
+            </div>
+          </div>
+          <div className="top-section-amount">
+            <p>{amount}</p>
+            <p>Total Awards</p>
           </div>
         </section>
         <section>
-          <ReactMarkdown children={fields.readmeContent} />
+          <Tabs>
+            <TabList>
+              <Tab>Details</Tab>
+              <Tab>FAQ</Tab>
+            </TabList>
+
+            <TabPanel>
+              <ReactMarkdown children={fields.readmeContent} />
+            </TabPanel>
+            <TabPanel>
+              <h2>FAQ</h2>
+              <p>
+                <strong>
+                  Corvids love to play pranks on humans and other animals?
+                </strong>
+                Corvids use their intelligence and ability to mimic sounds for
+                their own personal amusement. One zookeeper noted the magpies
+                would mimic the voice of the employee responsible for feeding
+                the chickens. The chickens would come running, but there would
+                be no food. The magpies would do this again and again to the
+                chickens, who never got wise to the prank.
+              </p>
+            </TabPanel>
+          </Tabs>
         </section>
       </article>
     </DefaultLayout>
