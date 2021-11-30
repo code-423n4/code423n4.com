566a93fe9d922b82d83156b8e5d53d46eba0357e
Diana <diana@dianaperkinsdesign.com>
2021-11-30 17:41:30 -0500

    adding working judge application submission server function

--- functions/apply-for-judge.js
+++ functions/apply-for-judge.js
@@ -1,43 +1,51 @@
 const { Octokit } = require("@octokit/core");
-const { token, apiKey, domain } = require("./_config");
+const { token } = require("./_config");
 
 const octokit = new Octokit({ auth: token });
-// const mg = require("mailgun-js")({ apiKey, domain });
 
 exports.handler = async (event) => {
-  console.log("event", event);
+  console.log("event:", event);
   // only allow POST
-  // if (event.httpMethod !== "POST") {
-  //   return {
-  //     statusCode: 405,
-  //     body: "Method not allowed",
-  //     headers: { Allow: "POST" },
-  //   };
-  // }
-  const data = JSON.parse(event.body);
-  const { handle, title } = data;
+  try {
+    if (event.httpMethod !== "POST") {
+      return {
+        statusCode: 405,
+        body: "Method not allowed",
+        headers: { Allow: "POST" },
+      };
+    }
 
-  // let handle = "Burgertime";
-  // let title = "Tick tock, it's judge o clock";
+    const data = JSON.parse(event.body);
+    const { handle, details } = data;
 
-  // ensure we have the data we need
-  if (!handle || !title) {
+    // ensure we have the data we need
+    if (!handle || !details) {
+      return {
+        statusCode: 422,
+        body: "Handle and details are required.",
+      };
+    }
+
+    const createIssue = await octokit.request(
+      "POST /repos/{owner}/{repo}/issues",
+      {
+        owner: "code-423n4",
+        repo: "judges",
+        title: `[TEST PR; INVALID] Warden ${handle} has applied for judge`,
+        body: `${details}`,
+        labels: ["invalid"],
+      }
+    );
+
     return {
-      statusCode: 422,
-      body: "Handle and title are required.",
+      statusCode: 201,
+      body: JSON.stringify({ createIssue }),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal server error." }),
     };
   }
-
-  // try {
-  //   let createIssue = await octokit.request(
-  //     "POST /repos/{owner}/{repo}/issues",
-  //     {
-  //       owner: "code-423n4",
-  //       repo: "judges",
-  //       title: `handle: ${handle} | title: ${title} `,
-  //     }
-  //   );
-  // } catch (err) {
-  //   console.log(`ERROR:${err}`);
-  // }
 };
--- src/pages/judge-application.js
+++ src/pages/judge-application.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useState, useRef } from "react";
-import { graphql } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
+import { omit, find } from "lodash";
 import clsx from "clsx";
 import HCaptcha from "@hcaptcha/react-hcaptcha";
 import DefaultLayout from "../layouts/DefaultLayout";
@@ -7,58 +8,166 @@ import * as styles from "../components/reporter/Form.module.scss";
 import * as widgetStyles from "../components/reporter/widgets/Widgets.module.scss";
 import { Widgets } from "../components/reporter/widgets";
 
-export default function JudgeApplication() {
+const config = {
+  labelAll: "candidate",
+  fields: [
+    {
+      name: "handle",
+      label: "Handle",
+      helptext: "Handle you're competing under",
+      widget: "warden",
+      required: true,
+      options: [],
+    },
+    {
+      name: "details",
+      label: "Vulnerability details",
+      helptext: "Link to all referenced sections of code in GitHub",
+      widget: "textarea",
+      required: true,
+    },
+  ],
+};
+
+const initialState = {
+  handle: "",
+  details: "",
+};
+
+const FormStatus = {
+  Unsubmitted: "unsubmitted",
+  Submitting: "submitting",
+  Submitted: "submitted",
+  Error: "error",
+};
+
+const wardenListQuery = graphql`
+  query WardenList {
+    allHandlesJson(sort: { fields: handle, order: ASC }) {
+      edges {
+        node {
+          id
+          handle
+          image {
+            childImageSharp {
+              resize(width: 64, quality: 90) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const JudgeApplication = () => {
   const [state, setState] = useState({
     handle: "",
-    title: "",
+    details: "",
   });
-  const url = `/.netlify/functions/apply-for-judge`;
-  const formData = {
-    handle: state.handle,
-    title: state.title,
-  };
+  const [status, setStatus] = useState("unsubmitted");
+
+  const fields = config.fields;
+
   const handleChange = useCallback((e) => {
     const { name, value } = e.target;
     setState((state) => {
       return { ...state, [name]: value };
     });
   }, []);
-  const applyForJudge = () => {
+
+  const url = `/.netlify/functions/apply-for-judge`;
+
+  const applyForJudge = useCallback((url, data) => {
     (async () => {
+      setStatus(FormStatus.Submitting);
       const response = await fetch(url, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(data),
       });
       if (response.ok) {
-        console.log("yep");
+        console.log("success");
+        setStatus(FormStatus.Submitted);
       } else {
         console.log("nope");
-        try {
-          const res = await response.json();
-          if (res.error) {
-            console.log("not this either");
-          }
-        } catch (err) {
-          console.log("super no");
-        }
+
+        setStatus(FormStatus.Error);
+        const message = `Error: ${response.status}`;
+        console.log(message);
       }
     })();
+  }, []);
+
+  // what is this
+  const bodyFields = omit(
+    state,
+    "title",
+    "status",
+    "label",
+    "email",
+    "address"
+  );
+
+  let markdownBody = [];
+
+  Object.keys(bodyFields).forEach((key) => {
+    const fieldOpts = find(fields, { name: key });
+    const input = bodyFields[key];
+    markdownBody.push(`# ${fieldOpts.label}\n\n${input}\n\n`);
+  });
+
+  const labelSet = [
+    config.labelAll ? config.labelAll : "",
+    state.label ? state.label : "",
+  ];
+
+  const formData = {
+    handle: state.handle,
+    details: state.details,
+  };
+  const handleSubmit = () => {
+    applyForJudge(url, formData);
   };
   return (
-    <DefaultLayout pageTitle="Judge Application | Code 423n4">
-      <div className="wrapper-main">
-        <h1 className="page-header">Judge Application</h1>
-        <form>
-          <input type="text" name="handle" onChange={handleChange}></input>
-          <input type="text" name="title" onChange={handleChange}></input>
-          <button type="button" onClick={applyForJudge}>
-            sdiufhisuh
-          </button>
-        </form>
-      </div>
-    </DefaultLayout>
+    <StaticQuery
+      query={wardenListQuery}
+      render={(data) => {
+        const wardens = data.allHandlesJson.edges.map(({ node }) => {
+          return { value: node.handle, image: node.image };
+        });
+        fields[0].options = wardens;
+
+        return (
+          <DefaultLayout
+            bodyClass="judge-application"
+            pageTitle="Judge Application | Code 423n4"
+          >
+            <div className="wrapper-main">
+              <h1 className="page-header">Judge Application</h1>
+              <form>
+                <Widgets
+                  fields={fields}
+                  onChange={handleChange}
+                  fieldState={state}
+                />
+                <button
+                  className="button cta-button"
+                  type="button"
+                  onClick={handleSubmit}
+                >
+                  Apply for Judge
+                </button>
+              </form>
+            </div>
+          </DefaultLayout>
+        );
+      }}
+    />
   );
-}
+};
+
+export default JudgeApplication;
