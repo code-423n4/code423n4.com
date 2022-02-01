5e0401a607611cffacee4b8dd905e4c71f110d26
Diana Perkins <Diana@dianaperkinsdesign.com>
2022-02-01 16:21:04 -0500

    adding message about QA submission changes

--- src/components/reporter/widgets/SelectField.js
+++ src/components/reporter/widgets/SelectField.js
@@ -2,32 +2,54 @@ import React, { useState } from "react";
 import clsx from "clsx";
 import * as styles from "./Widgets.module.scss";
 
-const SelectField = ({ name, options, onChange, isInvalid }) => {
-  const [value, setValue] = useState("")
+const SelectField = ({ name, options, onChange, isInvalid, contest }) => {
+  const [value, setValue] = useState("");
+  const [qaMessage, setQaMessage] = useState(false);
 
   const handleChange = (e) => {
     setValue(e.target.value);
     onChange(e);
-  }
+    const { name, value } = e.target;
+
+    if (
+      (name === "risk" && value.slice(0, 1) <= 1) ||
+      value.slice(0, 1) === "G"
+    ) {
+      setQaMessage(true);
+    } else {
+      setQaMessage(false);
+    }
+  };
 
   return (
-    <select
-      className={clsx(
-        styles.Control,
-        styles.Select,
-        isInvalid && "input-error",
-        value === "" && styles.Placeholder,
+    <>
+      {qaMessage && (
+        <div>
+          <p className="warning-message">
+            👋 Hi there! We've changed the way we are handling low risk,
+            non-critical, and gas optimization findings. Please submit them all
+            as one report. Check out <a href="">the FAQ</a> for more details.
+          </p>
+        </div>
       )}
-      name={name}
-      onChange={handleChange}
-    >
-      <option value="">Select...</option>
-      {options.map((option, index) => (
-        <option key={"option-" + index} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
+      <select
+        className={clsx(
+          styles.Control,
+          styles.Select,
+          isInvalid && "input-error",
+          value === "" && styles.Placeholder
+        )}
+        name={name}
+        onChange={handleChange}
+      >
+        <option value="">Select...</option>
+        {options.map((option, index) => (
+          <option key={"option-" + index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
 
