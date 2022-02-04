7564303b9ac841dfb6f15e4d8e2d2f9cdf8a7aaa
Captain Mango <captainmango@code423n4.com>
2022-02-04 14:09:57 -0800

    Fix lines of code in markdown body

--- src/components/reporter/Form.js
+++ src/components/reporter/Form.js
@@ -191,10 +191,9 @@ const Form = ({ contest, sponsor, repoUrl }) => {
   const [errorMessage, setErrorMessage] = useState("An error occurred");
   const [isQaOrGasFinding, setIsQaOrGasFinding] = useState(false);
 
+  const locString = state.linesOfCode.map((loc) => loc.value).join("\n");
   const details = isQaOrGasFinding ? state.qaGasDetails : state.details;
-  const markdownBody = `# LOC\n\n${state.linesOfCode.join(
-    "\n"
-  )}\n\n\n# Vulnerability details\n\n${details}\n\n`;
+  const markdownBody = `# LOC\n\n${locString}\n\n\n# Vulnerability details\n\n${details}\n\n`;
   const labelSet = [config.labelAll, state.risk ? state.risk : ""];
   const submissionUrl = `/.netlify/functions/submit-finding`;
   let title = "";
