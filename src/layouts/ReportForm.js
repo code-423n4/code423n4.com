0b075bc013151787a0df01381884bfa067213752
Captain Mango <captainmango@code423n4.com>
2022-02-14 17:18:01 -0800

    Prevent late submissions

--- src/layouts/ReportForm.js
+++ src/layouts/ReportForm.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import Form from "../components/reporter/Form";
 
 const ReportForm = (props) => {
+  const endTime = props.data.contestsCsv.end_time;
+  const hasContestEnded = Date.now() > new Date(endTime).getTime();
+
   return (
-    <div>
-      <Form
-        repoUrl={props.data.contestsCsv.findingsRepo}
-        contest={props.data.contestsCsv.contestid}
-        sponsor={props.data.contestsCsv.sponsor.name}
-      />
-    </div>
+    <main>
+      {hasContestEnded ? (
+        <div className="center">
+          <h1>This contest has ended.</h1>
+          <p>You can no longer submit findings for this contest.</p>
+          <Link
+            to="/contests"
+            className="contest-repo button button-small cta-button primary"
+          >
+            View active contests
+          </Link>
+        </div>
+      ) : (
+        <Form
+          repoUrl={props.data.contestsCsv.findingsRepo}
+          contest={props.data.contestsCsv.contestid}
+          sponsor={props.data.contestsCsv.sponsor.name}
+        />
+      )}
+    </main>
   );
 };
 
