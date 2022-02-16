fd0aa702966e6b371badf41ce180df1d5d5d27ec
Joshua Short <joshua.short@gmail.com>
2022-02-15 16:20:05 -0800

    Collect reports under _data

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/_reports`,
+        path: `${__dirname}/_data/reports`,
         name: `reports`,
       },
     },
