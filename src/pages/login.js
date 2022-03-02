aba51b961eed9493570671308ceffb782699795e
Joshua Short <joshua.short@gmail.com>
2022-03-02 04:52:45 -0800

    login placeholders

---
+++ src/pages/login.js
@@ -0,0 +1,17 @@
+import React, { useCallback, useState } from "react";
+
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+
+import DefaultLayout from "../templates/DefaultLayout";
+
+
+function LoginPageAAAA() {
+  return (
+    <DefaultLayout>
+      <h1>Sup!</h1>
+    </DefaultLayout>
+  );
+}
+
+export default LoginPageAAAA;
