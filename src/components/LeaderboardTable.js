b7b7b6f31e921ddd2b1deb83a899596f89eeba7f
Joshua Short <joshua.short@gmail.com>
2022-03-07 17:00:26 -0800

    Hack and copy for sponsor table

--- src/components/LeaderboardTable.js
+++ src/components/LeaderboardTable.js
@@ -19,6 +19,7 @@ const LeaderboardTable = ({ results, mode="warden" }) => {
             ),
           } : mode === "contest" ? {
             Header: "Contest",
+            accessor: "title",
           } : {},
           {
             Header: "USD",
--- src/templates/SponsorLayout.js
+++ src/templates/SponsorLayout.js
@@ -4,14 +4,58 @@ import { graphql } from "gatsby";
 import DefaultLayout from "./DefaultLayout";
 import LeaderboardTable from "../components/LeaderboardTable";
 
+function updateFindings(data, finding) {
+  data.allFindings += 1;
+  data.awardTotal += finding.awardUSD ?? 0;
+
+  switch (finding.risk.toLowerCase()) {
+    case "0":
+      data.nonCrit += 1;
+      break;
+    case "1":
+      data.lowRisk += 1;
+      break;
+    case "2":
+      data.medRisk += 1;
+      break;
+    case "3":
+      data.highRisk += 1;
+      break;
+    case "g":
+      data.gasOptz += 1;
+      break;
+    default:
+      break;
+  }
+}
+
 const SponsorLayout = (props) => {
   const pageTitle = `Sponsor ${props.pageContext.sponsorId}`;
 
   const resultData = useMemo(() => {
     let resultData = [];
 
+    for (const contest of props.data.contestFindings.contests) {
+      let contestData = {
+        title: `${contest.title} - ${contest.start_time}`,
+        image: "",
+        link: "",
+        lowRisk: 0,
+        medRisk: 0,
+        highRisk: 0,
+        nonCrit: 0,
+        gasOptz: 0,
+        allFindings: 0,
+        awardTotal: 0,
+      };
+      for (const finding of contest.findings) {
+        updateFindings(contestData, finding);
+      }
+      resultData.push(contestData);
+    }
+
     return resultData;
-  });
+  }, [props.data.contestFindings]);
 
   return (
     <DefaultLayout
@@ -34,11 +78,14 @@ query SponsorStatsQuery($sponsorId: String) {
     name
     contests {
       title
+      start_time
       fields {
         contestPath
       }
       findings {
         finding
+        risk
+        awardUSD
         handle {
           handle
         }
