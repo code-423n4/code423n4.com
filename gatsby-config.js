bc9c2ca14b705f5134d15f95c893c934b1b7d550
Joshua Short <joshua.short@gmail.com>
2022-04-30 01:45:20 -0700

    Move _content to _data/pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/_content`,
+        path: `${__dirname}/_data/pages`,
         name: `pages`,
       },
     },
