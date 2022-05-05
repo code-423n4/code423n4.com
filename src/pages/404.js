0f0816bdcfe793204291fb7da06c0fae8c2ee747
simonbusch <Simon.busch@live.fr>
2022-05-05 13:09:34 +0200

    [UPD] change place holder for 404 C4 meme, upd style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,23 @@
 import React, { useEffect } from "react";
 import DefaultLayout from "../templates/DefaultLayout";
-import { navigate } from "gatsby"
+import { navigate } from "gatsby";
 
 export default function ErrorPage() {
   useEffect(() => {
     setTimeout(() => {
       navigate("/", { replace: true });
-    }, 4000)
+    }, 3000);
   }, []);
 
   return (
     <DefaultLayout bodyClass="landing" key={"404 page"}>
       <div className="container-404">
         <h1>Page not found 🐺</h1>
-        <p>
-          It seems that something is wrong. If you feel like this page should
-          exist, please reach out to the team.
-        </p>
-        <img src="/images/404.jpg" alt="Code 423n4" className="placeholder" />
+        <img
+          src="/images/c4-404-meme.png"
+          alt="404 meme"
+          className="placeholder"
+        />
       </div>
     </DefaultLayout>
   );
