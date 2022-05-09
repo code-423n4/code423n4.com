6790f2f8627ebb76a8dd041365a687c2a82230f8
Simon Busch <Simon.busch@live.fr>
2022-05-09 20:58:26 +0200

    Update src/components/reporter/Form.js
    Co-authored-by: joshuashort <joshua.short@gmail.com>

--- src/components/reporter/Form.js
+++ src/components/reporter/Form.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import clsx from "clsx";
 import { StaticQuery, graphql } from "gatsby";
 
