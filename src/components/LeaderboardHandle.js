c9976f8f4c32c0769f06e3e3d8f34b202b6e0fe3
simonbusch <Simon.busch@live.fr>
2022-05-19 14:31:08 +0200

    [ADD] toggle for teams

--- src/components/LeaderboardHandle.js
+++ src/components/LeaderboardHandle.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "react-avatar";
 
 const LeaderboardHandle = ({ handle, image, link, members }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   let mobileView = false;
   if (typeof window !== "undefined") {
     mobileView = window.innerWidth <= 740 ? true : false;
@@ -18,30 +20,37 @@ const LeaderboardHandle = ({ handle, image, link, members }) => {
   return (
     <div className="wrapper-competitor" key={handle}>
       {members ? (
-        <div className="wrapper-members">
-          <div className="team-wrapper">
-            <span
-              className={
-                mobileView === false ? "team-avatar" : "team-avatar-small"
-              }
-            >
-              {handle.substring(0, 1)}
-            </span>
-            <span className="team-name">{handle}</span>
-          </div>
-          {members.map((member) => (
-            <div className="member team-identifier" key={member.handle}>
-              <a href={member.link}>
-                <Avatar
-                  src={member.image && member.image.childImageSharp.resize.src}
-                  name={member.handle}
-                  size="13px"
-                  round="8px"
-                />
-                <span>{member.handle}</span>
-              </a>
+        <div className="wrapper-members" onClick={() => setIsExpanded(!isExpanded)}>
+          <div className="wrapper-team-top">
+            <div className="team-toggle">{isExpanded ? "▼ " : "▶ "}</div>
+            <div className="team-wrapper">
+              <span
+                className={
+                  mobileView === false ? "team-avatar" : "team-avatar-small"
+                }
+              >
+                {handle.substring(0, 1)}
+              </span>
+              <span className="team-name">{handle}</span>
             </div>
-          ))}
+          </div>
+          {isExpanded
+            ? members.map((member) => (
+                <div className="member team-identifier" key={member.handle}>
+                  <a href={member.link}>
+                    <Avatar
+                      src={
+                        member.image && member.image.childImageSharp.resize.src
+                      }
+                      name={member.handle}
+                      size={mobileView === false ? "25px" : "13px"}
+                      round={mobileView === false ? "25px" : "8px"}
+                    />
+                    <span>{member.handle}</span>
+                  </a>
+                </div>
+              ))
+            : ""}
         </div>
       ) : (
         <a href={link}>
