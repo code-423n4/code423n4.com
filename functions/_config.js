26778cfc8622d2c1f711709c83de2a345f6c9dd3
simonbusch <Simon.busch@live.fr>
2022-05-26 06:19:16 +0200

    [UPD] add first tests to retrieve info from notion db

--- functions/_config.js
+++ functions/_config.js
@@ -4,4 +4,6 @@ module.exports = {
   token: process.env.GITHUB_TOKEN,
   apiKey: process.env.MAILGUN_KEY,
   domain: process.env.MAILGUN_DOMAIN,
+  notionToken: process.env.INTEGRATION_TOKEN,
+  notionDb: process.env.DATABASE_ID,
 };
--- functions/getNotionData.js
+++ functions/getNotionData.js
@@ -1,1 +1,11 @@
-// get all data from notion DB
+const { Client } = require("@notionhq/client");
+const { notionToken, notionDb } = require("./_config");
+const notion = new Client({ auth: notionToken });
+
+(async () => {
+
+  const response = await notion.databases.query({
+    database_id: notionDb,
+  });
+  console.log(response.results[0].properties);
+})();
