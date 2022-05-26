0ae235e52085c31f2b062085666a22fe03ef62b4
simonbusch <Simon.busch@live.fr>
2022-05-26 13:29:07 +0200

    [UPD] fix logic

--- functions/getNotionData.js
+++ functions/getNotionData.js
@@ -3,39 +3,48 @@ const { notionToken, notionDb } = require("./_config");
 const notion = new Client({ auth: notionToken });
 
 (async () => {
-  const pages = []
-  let cursor = undefined
+  const pages = [];
+  let cursor = undefined;
   while (true) {
     const { results, next_cursor } = await notion.databases.query({
       database_id: notionDb,
       start_cursor: cursor,
-    })
-    pages.push(...results)
+    });
+    pages.push(...results);
     if (!next_cursor) {
-      break
+      break;
     }
-    cursor = next_cursor
+    cursor = next_cursor;
   }
-  console.log(`${pages.length} results successfully fetched.`)
-  const tempResponse =  pages.map(page => {
-    if (page.properties.Status.select?.name === "Lost deal" || page.properties.Status.select?.name === "Possible") {
-      return null;
-    } else {
-      return {
-        contestId: page.properties.ContestID.number || null,
-        status: page.properties.Status.select?.name || null,
-      }
-    }
-  }).filter(el => el !== null);
-  console.log(tempResponse, tempResponse.length)
 
+  const tempResponse = pages
+    .map((page) => {
+      // !! Problem
+      // 15 Gro Protocol ??
+      // 33 PoolTogether ??
+      // 46 Silo Finance ??
+      if (
+        page.properties.Status.select?.name === "Lost deal" ||
+        page.properties.Status.select?.name === "Possible" ||
+        !page.properties.Status.select?.name ||
+        !page.properties.ContestID?.number
+      ) {
+        return null;
+      } else {
+        return {
+          contestId: page.properties.ContestID.number || null,
+          status: page.properties.Status.select?.name || null,
+        };
+      }
+    })
+    .filter((el) => el !== null);
 
   function sortByKey(array, key) {
-    return array.sort(function(a, b) {
-        var x = a[key]; var y = b[key];
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    return array.sort(function (a, b) {
+      var x = a[key];
+      var y = b[key];
+      return x < y ? -1 : x > y ? 1 : 0;
     });
   }
-  console.log(sortByKey(tempResponse, "contestId"));
-
+  return sortByKey(tempResponse);
 })();
