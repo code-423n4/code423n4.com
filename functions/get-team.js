f87ca5b906a0a3942eb702eb8ed151f037bd54ad
simonbusch <Simon.busch@live.fr>
2022-06-01 08:45:32 +0200

    [UPD] create get team lambda function and WIP

---
+++ functions/get-team.js
@@ -0,0 +1,42 @@
+const { readFileSync } = require("fs");
+const path = require("path");
+const fs = require("fs");
+
+exports.handler = async (event) => {
+  // only allow GET
+  if (event.httpMethod !== "GET") {
+    return {
+      statusCode: 405,
+      body: "Method not allowed",
+      headers: { Allow: "GET" },
+    };
+  }
+  const userHandle = event.queryStringParameters.id;
+
+  try {
+    const handleDir = path.join(__dirname + "/../_data/handles");
+    let data = fs.readdirSync(handleDir);
+    const teams = data
+      .map((handle) => {
+        if (handle.includes(".json")) {
+          const warden = readFileSync(`./_data/handles/${handle}`);
+          return JSON.parse(warden.toString());
+        } else {
+          return null;
+        }
+      })
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(teams),
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: "Team not found" }),
+    };
+  }
+};
+
+test();
