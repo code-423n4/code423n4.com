7d5c6f6962f72ad6fb0597ca675ea863eed91b6b
Joshua Short <joshua.short@gmail.com>
2022-06-13 11:47:01 -0700

    wip: pieces

--- _config.js
+++ _config.js
@@ -1,7 +1,38 @@
 require("dotenv").config();
 
+let prefix = "";
+
+switch (process.env.C4_ENV) {
+  case "production":
+    prefix = "PRODUCTION_";
+    break;
+
+  case "staging":
+    prefix = "STAGING_";
+    break;
+
+  default:
+    break;
+}
+// @todo: figure out how to make sure these functions use the right env variables
+// const environment = process.env.NODE_ENV;
+// const isStaging = process.env.GATSBY_STAGING;
+// let moralisServerUrl;
+// let moralisAppId;
+
+// if (isStaging) {
+//   moralisServerUrl = process.env.GATSBY_STAGING_MORALIS_SERVER;
+//   moralisAppId = process.env.GATSBY_STAGING_MORALIS_APP_ID;
+// } else if (environment === "production") {
+//   moralisServerUrl = process.env.GATSBY_PRODUCTION_MORALIS_SERVER;
+//   moralisAppId = process.env.GATSBY_PRODUCTION_MORALIS_APP_ID;
+// } else {
+//   moralisServerUrl = process.env.GATSBY_DEV_MORALIS_SERVER;
+//   moralisAppId = process.env.GATSBY_DEV_MORALIS_APP_ID;
+// }
+
 module.exports = {
-  token: process.env.GITHUB_TOKEN,
+  token: process.env[`${prefix}GITHUB_TOKEN`],
   apiKey: process.env.MAILGUN_KEY,
   domain: process.env.MAILGUN_DOMAIN,
   moralisServerUrl: process.env.GATSBY_MORALIS_SERVER,
--- netlify/plugins/netlify-plugin-env-config/index.js
+++ netlify/plugins/netlify-plugin-env-config/index.js
@@ -1,5 +1,56 @@
-module.exports = {
-  onPreBuild: () => {
-    console.log("PRE BUILD");
-  },
+// https://www.netlify.com/blog/2020/05/21/localize-your-environment-variables-by-context-with-build-plugins/
+// https://github.com/cball/netlify-plugin-contextual-env/blob/master/src/index.js
+
+// module.exports = {
+//   onPreBuild: () => {
+//     console.log("PRE BUILD");
+//   },
+// }
+
+// const fs = require('fs');
+// const util = require('util');
+
+/**
+ * Overrides an ENV var with a value if it exists
+ * @param {*} key the key to overwrite if found
+ * @param {*} contextOrBranch the value to check
+ * @param {*} mode the mode to use (prefix or suffix)
+ */
+function setEnvWithValue(key, contextOrBranch, mode) {
+  const envVar = mode === 'prefix' ? `${contextOrBranch}_${key}` : `${key}_${contextOrBranch}`;
+
+  if (!process.env[envVar]) {
+    return;
+  }
+
+  // console.log(`Exporting ${key}=${process.env[envVar]}.`);
+  console.log(`Exporting ${key} <- ${envVar}.`);
+  process.env[key] = process.env[envVar];
+
+  return `${key}=${process.env[envVar]}\n`;
 }
+
+module.exports = {
+  onPreBuild: async ({ inputs }) => {
+    const context = `${process.env.CONTEXT}`.toUpperCase().replace(/-/g, '_');
+    const branch = `${process.env.BRANCH}`.toUpperCase().replace(/-/g, '_');
+    const { mode } = inputs;
+
+    Object.keys(process.env).forEach((key) => {
+      console.log(`${key}=${process.env[key]}`);
+    });
+
+    const envOverrides = Object.keys(process.env).map((key) => [
+      setEnvWithValue(key, context, mode),
+      setEnvWithValue(key, branch, mode),
+    ]);
+
+    const replaced = [].concat(...envOverrides).filter(Boolean);
+
+    if (replaced.length) {
+      console.log(`Replaced ${replaced.length} ENVs`);
+    } else {
+      console.log(`Nothing found... keeping default ENVs`);
+    }
+  },
+};
