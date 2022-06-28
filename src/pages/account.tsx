7d53f2fbc2a6a65082327efe5edfe066f78d5996
Captain Mango <captainmango@code423n4.com>
2022-06-27 23:32:28 -0700

    Remove ability to change username

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -4,12 +4,14 @@ import React, { useEffect, useState } from "react";
 import { useMoralis } from "react-moralis";
 import { toast } from "react-toastify";
 
-import { useModalContext } from "../hooks/ModalContext";
+// hooks
 import useUser from "../hooks/UserContext";
 
+// components
 import { Input } from "../components/Input";
 import ProtectedPage from "../components/ProtectedPage";
 
+// styles
 import * as styles from "../components/form/Form.module.scss";
 import * as inputStyles from "../components/Input.module.scss";
 
@@ -29,19 +31,12 @@ enum FormStatus {
 export default function ConfirmAccount() {
   // hooks
   const { isInitialized, isInitializing, user } = useMoralis();
-  const { currentUser, reFetchUser, logUserOut } = useUser();
-  const { showModal } = useModalContext();
+  const { currentUser, reFetchUser } = useUser();
 
   // state
   const [state, setState] = useState<Record<string, string>>(initialState);
-  const [username, setUsername] = useState<string>("");
   const [addresses, setAddresses] = useState<string[]>([]);
-  const [wardenInfoStatus, setWardenInfoFormStatus] = useState<FormStatus>(
-    FormStatus.Unsubmitted
-  );
-  const [usernameFormStatus, setUsernameFormStatus] = useState<FormStatus>(
-    FormStatus.Unsubmitted
-  );
+  const [status, setStatus] = useState<FormStatus>(FormStatus.Unsubmitted);
 
   const getUser = async (): Promise<void> => {
     const { discordUsername, gitHubUsername, emailAddress } = currentUser;
@@ -55,7 +50,6 @@ export default function ConfirmAccount() {
       return;
     }
     getUser();
-    resetUsername();
   }, [currentUser, isInitialized]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -78,19 +72,6 @@ export default function ConfirmAccount() {
     return [];
   };
 
-  const validateUsername = (value) => {
-    const errors = [];
-    if (value.length > 25) {
-      errors.push("Length is limited to 25 characters.");
-    }
-    if (value.match(/^[0-9a-zA-Z_\-]+$/) === null) {
-      errors.push(
-        "Can only contain alphanumeric characters, underscores, and hyphens"
-      );
-    }
-    return errors;
-  };
-
   const handleSaveWardenInfo = async () => {
     if (!user) {
       return;
@@ -111,8 +92,7 @@ export default function ConfirmAccount() {
       return;
     }
 
-    setWardenInfoFormStatus(FormStatus.Submitting);
-
+    setStatus(FormStatus.Submitting);
     user.set("discordUsername", state.discordUsername);
     user.set("gitHubUsername", state.gitHubUsername);
     user.set("emailAddress", state.emailAddress);
@@ -121,100 +101,15 @@ export default function ConfirmAccount() {
       await user.save();
       await reFetchUser();
       toast.success("Your information has been updated");
-      setWardenInfoFormStatus(FormStatus.Submitted);
+      setStatus(FormStatus.Submitted);
     } catch (error) {
-      setWardenInfoFormStatus(FormStatus.Error);
-      toast.error("An error occurred. Your changes have not been saved");
-    }
-  };
-
-  const handleSaveUsername = async () => {
-    if (username === currentUser.username) {
-      return;
-    }
-
-    const usernameValidationErrors = validateUsername(username);
-    if (!username || usernameValidationErrors.length > 0) {
-      return;
-    }
-
-    setUsernameFormStatus(FormStatus.Submitting);
-    showModal({
-      title: `Are you sure you want to change your username to "${username}"?`,
-      body: (
-        <>
-          <ul>
-            <h3>If you change your username:</h3>
-            <li>
-              Your previous leaderboard stats will still be linked to the name "
-              {currentUser.username}"
-            </li>
-            <li>
-              Your previous findings in contest reports will still be attributed
-              to "{currentUser.username}"
-            </li>
-            <li>
-              You will be blocked from submitting findings for a short period of
-              time while we review and approve your request.
-            </li>
-          </ul>
-        </>
-      ),
-      primaryButtonText: "Change username",
-      primaryButtonAction: async () => await saveUsername(),
-      secondaryButtonAction: async () =>
-        setUsernameFormStatus(FormStatus.Unsubmitted),
-    });
-  };
-
-  const saveUsername = async () => {
-    try {
-      const url = `/.netlify/functions/update-user`;
-      const requestBody = {
-        newUsername: username,
-        username: currentUser.username,
-        polygonAddress: currentUser.address,
-        gitHubUsername: currentUser.gitHubUsername,
-      };
-
-      const sessionToken = user.attributes.sessionToken;
-
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "X-Authorization": `Bearer ${sessionToken}`,
-        },
-        body: JSON.stringify(requestBody),
-      });
-
-      if (!response.ok) {
-        const { error } = await response.json();
-        console.error(error);
-        throw "Saving username failed";
-      }
-
-      user.set("c4Username", username);
-      await user.save();
-
-      toast.success(
-        "Your request to change your username has been submitted. You have been logged out."
-      );
-      await logUserOut();
-      setUsernameFormStatus(FormStatus.Submitted);
-    } catch (error) {
-      setUsernameFormStatus(FormStatus.Error);
+      setStatus(FormStatus.Error);
       toast.error("An error occurred. Your changes have not been saved");
     }
   };
 
   const resetWardenInfo = (): void => {
     getUser();
-  };
-
-  const resetUsername = (): void => {
-    const { username } = currentUser;
-    setUsername(username);
   };
 
   return (
@@ -260,7 +155,7 @@ export default function ConfirmAccount() {
                 type="button"
                 className="button cta-button secondary"
                 onClick={resetWardenInfo}
-                disabled={wardenInfoStatus === FormStatus.Submitting}
+                disabled={status === FormStatus.Submitting}
               >
                 Clear Changes
               </button>
@@ -268,9 +163,9 @@ export default function ConfirmAccount() {
                 type="button"
                 className="button cta-button"
                 onClick={handleSaveWardenInfo}
-                disabled={wardenInfoStatus === FormStatus.Submitting}
+                disabled={status === FormStatus.Submitting}
               >
-                {wardenInfoStatus === FormStatus.Submitting
+                {status === FormStatus.Submitting
                   ? "Saving..."
                   : "Save Changes"}
               </button>
@@ -296,51 +191,6 @@ export default function ConfirmAccount() {
                 Create a new team
               </Link>
             </div>
-            <fieldset className={styles.EmphasizedInputGroup}>
-              <h2 className={styles.Heading2}>Username</h2>
-              <p className="warning-message">
-                <p>
-                  🚨 WARNING: your code4rena username is tied to any findings
-                  you may have submitted in the past. If you change your
-                  username, your old username will still be linked to your
-                  previous findings on the leaderboard and in past reports.
-                  Changing your username will effectively be like starting over.
-                </p>
-                <p>
-                  Additionally, changing your username requires approval from
-                  the the code4rena team. Once you submit the request, you will
-                  be blocked from submitting findings until the change has been
-                  reviewed and approved.
-                </p>
-              </p>
-              <Input
-                label="Code4rena Username:"
-                handleChange={(e) => setUsername(e.target.value)}
-                value={username}
-                name="username"
-                required={true}
-                validator={validateUsername}
-              />
-              <div className={styles.ButtonsWrapper}>
-                <button
-                  type="button"
-                  className="button cta-button secondary"
-                  onClick={resetUsername}
-                  disabled={usernameFormStatus === FormStatus.Submitting}
-                >
-                  Clear Changes
-                </button>
-                <button
-                  type="button"
-                  className="button cta-button danger"
-                  onClick={handleSaveUsername}
-                >
-                  {usernameFormStatus === FormStatus.Submitting
-                    ? "Saving..."
-                    : "Save Username"}
-                </button>
-              </div>
-            </fieldset>
           </form>
         </div>
       )}
