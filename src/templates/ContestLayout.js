821fd3c4a020255739165ef34c42e73c67c7a553
simonbusch <Simon.busch@live.fr>
2022-07-01 17:19:06 +0200

    [FIX] white space and identation

--- src/templates/ContestLayout.js
+++ src/templates/ContestLayout.js
@@ -65,27 +65,29 @@ const ContestLayout = (props) => {
             )}
             <p className="days-duration">{t.daysDuration} day contest</p>
           </div>
-          { fields.artPath !== null ? (
-          <img
-          src={fields.artPath}
-            onClick={() => setArtOpen((isOpen) => !isOpen)}
-            className={clsx(
-              { open: artOpen },
-              "contest-artwork background-pattern",
-              "button-div",
-            )}
-            aria-label={`${title} artwork. Expands on click.`}
-          />
-           ) : (<button
-            onClick={() => setArtOpen((isOpen) => !isOpen)}
-            className={clsx(
-              { open: artOpen },
-              "contest-artwork background-pattern ",
-              "button-div",
-              "contest-artwork-default"
-            )}
-            aria-label={`${title} artwork. Expands on click.`}
-          />)} 
+          {fields.artPath !== null ? (
+            <img
+              src={fields.artPath}
+              onClick={() => setArtOpen((isOpen) => !isOpen)}
+              className={clsx(
+                { open: artOpen },
+                "contest-artwork background-pattern",
+                "button-div"
+              )}
+              aria-label={`${title} artwork. Expands on click.`}
+            />
+          ) : (
+            <button
+              onClick={() => setArtOpen((isOpen) => !isOpen)}
+              className={clsx(
+                { open: artOpen },
+                "contest-artwork background-pattern ",
+                "button-div",
+                "contest-artwork-default"
+              )}
+              aria-label={`${title} artwork. Expands on click.`}
+            />
+          )}
         </div>
         <section className="top-section contest-wrapper">
           <div className="sponsor-image">
