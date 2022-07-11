cf0115a1773b9bf8f70a5c0e9683ca53e407e939
Joshua Short <joshua.short@gmail.com>
2022-07-11 06:20:52 -0700

    Add netlify response placeholder

--- functions/edit-finding.ts
+++ functions/edit-finding.ts
@@ -4,4 +4,9 @@ export async function handler(event) {
   // event.httpMethod
   //   GET: load finding(s)
   //   POST: write finding
+
+  return {
+    statusCode: 200,
+    body: "Some Response",
+  };
 };
