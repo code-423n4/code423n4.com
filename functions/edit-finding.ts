8ce3bbc5e967f27d6b8d35a92882564c6a87e8e6
Joshua Short <joshua.short@gmail.com>
2022-07-11 06:56:42 -0700

    The edit and get paths?

--- functions/edit-finding.ts
+++ functions/edit-finding.ts
@@ -1,19 +1,31 @@
 // wip: endpoint for self-serve submission editing
 
+function getFindings(req) {
+  // the request needs to come from an authenticated warden
+  // they can see their findings for active contests
+  // they can see their teams' findings for active contests
+  // there are some parameters?
+  return {
+    statusCode: 200,
+    body: "Get OK",
+  };
+}
+
+function editFinding(req) {
+  // an authenticated warden can edit a finding
+  //   for active contests
+  //     their own (their teams')
+  return {
+    statusCode: 200,
+    body: "Edit OK",
+  };
+}
+
 export async function handler(event) {
   switch (event.httpMethod) {
     case "GET":
-      // return a list of findings
-      //   - warden (+team)
-      //   - contest
-      break;
+      return getFindings(event);
     case "POST":
-      // modify a specific finding
-      break;
+      return editFinding(event);
   }
-
-  return {
-    statusCode: 200,
-    body: "Some Response",
-  };
 };
