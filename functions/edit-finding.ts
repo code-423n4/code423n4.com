947984b4d80c009ba3277907a68ef604b5736f7f
Joshua Short <joshua.short@gmail.com>
2022-07-11 06:17:29 -0700

    indentation

--- functions/edit-finding.ts
+++ functions/edit-finding.ts
@@ -1,7 +1,7 @@
 // wip: endpoint for self-serve submission editing
 
 export async function handler(event) {
-    // event.httpMethod
-    //   GET: load finding(s)
-    //   POST: write finding
+  // event.httpMethod
+  //   GET: load finding(s)
+  //   POST: write finding
 };
