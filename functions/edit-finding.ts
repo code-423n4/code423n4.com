121f67914987f8f3594d7274737d4364f2cbbc0c
Joshua Short <joshua.short@gmail.com>
2022-07-11 06:56:06 -0700

    start some logic

--- functions/edit-finding.ts
+++ functions/edit-finding.ts
@@ -1,9 +1,16 @@
 // wip: endpoint for self-serve submission editing
 
 export async function handler(event) {
-  // event.httpMethod
-  //   GET: load finding(s)
-  //   POST: write finding
+  switch (event.httpMethod) {
+    case "GET":
+      // return a list of findings
+      //   - warden (+team)
+      //   - contest
+      break;
+    case "POST":
+      // modify a specific finding
+      break;
+  }
 
   return {
     statusCode: 200,
