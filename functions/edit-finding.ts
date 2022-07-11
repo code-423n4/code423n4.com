28d2fbe6f344f6eb730c3eb16c10da0a4e826c73
Joshua Short <joshua.short@gmail.com>
2022-07-11 06:14:46 -0700

    Change def a bit

--- functions/edit-finding.ts
+++ functions/edit-finding.ts
@@ -1,6 +1,6 @@
 // wip: endpoint for self-serve submission editing
 
-exports.handler = async (event) => {
+export async function handler(event) {
     // event.httpMethod
     //   GET: load finding(s)
     //   POST: write finding
