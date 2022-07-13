e7d56942119e2caf33695abfc1cf4820bbfd30d0
Joshua Short <joshua.short@gmail.com>
2022-07-13 00:49:47 -0700

    wip: adapting auth from submit function (thanks 🥭!)

--- functions/manage-findings.ts
+++ functions/manage-findings.ts
@@ -1,3 +1,10 @@
+const { Moralis } = require("moralis/node");
+
+const {
+  moralisAppId,
+  moralisServerUrl,
+} = require("./_config");
+
 // wip: endpoint for self-serve submission editing
 
 function getFindings(req) {
@@ -26,6 +33,61 @@ function editFinding(req) {
 }
 
 export async function handler(event) {
+  const authorization = event.headers["x-authorization"];
+
+  if (!authorization) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        error: "Unauthorized",
+      }),
+    };
+  }
+
+  await Moralis.start({
+    serverUrl: moralisServerUrl,
+    appId: moralisAppId,
+  });
+
+  // wip: from submit-finding auth
+  //   do we need username? can we check session token?
+  // const userUrl = `${event.headers.origin}/.netlify/functions/get-user?id=${user}`;
+  // const userResponse = await fetch(userUrl);
+  // if (!userResponse.ok) {
+  //   return {
+  //     statusCode: 401,
+  //     body: JSON.stringify({
+  //       error: "You must be registered to submit findings",
+  //     }),
+  //   };
+  // }
+
+  // const userData = await userResponse.json();
+  // if (!userData || !userData.moralisId) {
+  //   return {
+  //     statusCode: 401,
+  //     body: JSON.stringify({
+  //       error: "You must be registered to submit findings",
+  //     }),
+  //   };
+  // }
+
+  // const { moralisId } = userData;
+  // const sessionToken = authorization.split("Bearer ")[1];
+  // const confirmed = await Moralis.Cloud.run("confirmUser", {
+  //   sessionToken,
+  //   moralisId,
+  //   username: user,
+  // });
+  // if (!confirmed) {
+  //   return {
+  //     statusCode: 401,
+  //     body: JSON.stringify({
+  //       error: "Authorization failed",
+  //     }),
+  //   };
+  // }
+
   switch (event.httpMethod) {
     case "GET":
       return getFindings(event);
