d180d0e59b886c09f9005508853f53ef79afddf3
Joshua Short <joshua.short@gmail.com>
2022-07-13 17:40:19 -0700

    these async, too

--- netlify/functions/manage-findings.ts
+++ netlify/functions/manage-findings.ts
@@ -11,7 +11,7 @@ const { getContestEnd } = require("../util/contest-utils");
 
 // wip: endpoint for self-serve submission editing
 
-function getFindings(req) {
+async function getFindings(req) {
   // the request needs to come from an authenticated warden
   // they can see their findings for active contests
   // they can see their teams' findings for active contests
@@ -29,7 +29,7 @@ function getFindings(req) {
   };
 }
 
-function editFinding(req) {
+async function editFinding(req) {
   // an authenticated warden can edit a finding
   //   for active contests
   //     their own (their teams')
@@ -97,9 +97,9 @@ const handler: Handler = async (event, context) => {
 
   switch (event.httpMethod) {
     case "GET":
-      return getFindings(event);
+      return await getFindings(event);
     case "POST":
-      return editFinding(event);
+      return await editFinding(event);
     default:
       return {
         statusCode: 418,
