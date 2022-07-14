3cf15ba7e53ee32b229105d8e132a17ca7e5feb0
Joshua Short <joshua.short@gmail.com>
2022-07-14 03:38:28 -0700

    utils for contest/active

--- netlify/util/contest-utils.ts
+++ netlify/util/contest-utils.ts
@@ -1,22 +1,27 @@
 import csv from "csvtojson";
 
 
-async function getContestEnd(contestId) {
-  // if (process.env.NODE_ENV === "development") {
-  //   contests = csv().fromFile("_test-data/contests/contests.csv");
-  // } else {
-  //   contests = csv().fromFile("_data/contests/contests.csv");
-  // }
-
+async function getContest(contestId) {
   const contests = await csv().fromFile("_data/contests/contests.csv");
   const contest = contests.find((c) => c.contestid == contestId);
 
-  if (contest) {
-    return new Date(contest.end_time).getTime();
-  }
-  // else throw?
+  return contest;
+}
+
+function isContestActive(contestId) {
+  return getContest(contestId).then((c) => {
+    if (c) {
+      const now = Date.now();
+      const start = new Date(c.start_time).getTime();
+      const end = new Date(c.end_time).getTime();
+  
+      return (now >= start) && (now <= end);
+    }
+    return false;
+  });
 }
 
 export {
-  getContestEnd,
+  getContest,
+  isContestActive,
 };
