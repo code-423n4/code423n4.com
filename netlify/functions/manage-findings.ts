7f673a5eb17efa81f7ef36c15007f1905e77690f
Joshua Short <joshua.short@gmail.com>
2022-07-14 00:36:13 -0700

    start auth utils

--- netlify/functions/manage-findings.ts
+++ netlify/functions/manage-findings.ts
@@ -1,13 +1,7 @@
 import { Handler } from "@netlify/functions";
 
-import Moralis from "moralis/node";
-
-const {
-  moralisAppId,
-  moralisServerUrl,
-} = require("../_config");
-
 import { getContestEnd } from "../util/contest-utils";
+import { checkAuth } from "../util/auth-utils";
 
 // wip: endpoint for self-serve submission editing
 
@@ -38,61 +32,6 @@ async function editFinding(req) {
 }
 
 const handler: Handler = async (event, context) => {
-  const authorization = event.headers["x-authorization"];
-
-  if (!authorization) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({
-        error: "Unauthorized",
-      }),
-    };
-  }
-
-  await Moralis.start({
-    serverUrl: moralisServerUrl,
-    appId: moralisAppId,
-  });
-
-  // wip: from submit-finding auth
-  //   do we need username? can we check session token?
-  // const userUrl = `${event.headers.origin}/.netlify/functions/get-user?id=${user}`;
-  // const userResponse = await fetch(userUrl);
-  // if (!userResponse.ok) {
-  //   return {
-  //     statusCode: 401,
-  //     body: JSON.stringify({
-  //       error: "You must be registered to submit findings",
-  //     }),
-  //   };
-  // }
-
-  // const userData = await userResponse.json();
-  // if (!userData || !userData.moralisId) {
-  //   return {
-  //     statusCode: 401,
-  //     body: JSON.stringify({
-  //       error: "You must be registered to submit findings",
-  //     }),
-  //   };
-  // }
-
-  // const { moralisId } = userData;
-  // const sessionToken = authorization.split("Bearer ")[1];
-  // const confirmed = await Moralis.Cloud.run("confirmUser", {
-  //   sessionToken,
-  //   moralisId,
-  //   username: user,
-  // });
-  // if (!confirmed) {
-  //   return {
-  //     statusCode: 401,
-  //     body: JSON.stringify({
-  //       error: "Authorization failed",
-  //     }),
-  //   };
-  // }
-
   switch (event.httpMethod) {
     case "GET":
       return await getFindings(event);
