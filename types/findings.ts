d4c84ced4732d529878eed3a032dd52d34d3ffca
Joshua Short <joshua.short@gmail.com>
2022-07-26 02:17:46 -0700

    Move to types

--- types/findings.ts
+++ types/findings.ts
@@ -12,6 +12,23 @@ export interface Finding {
   issueNumber: number;
 }
 
+export interface FindingEditRequest {
+  issue: number;
+  contest: number;
+  emailAddresses?: string[];
+  attributedTo?: {
+    newValue: string;
+    oldValue: string;
+    wallet: string;
+  };
+  risk?: {
+    newValue: string;
+    oldValue: string;
+  };
+  title?: string;
+  body?: string;
+}
+
 export interface FindingsResponse {
   user: Finding[];
   teams: Record<string, Finding[]>;
