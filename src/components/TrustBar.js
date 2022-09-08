82e7c62993a71e325053e2faecc0ab6a714f4afc
Adam Avenir <adamavenir@hey.com>
2022-09-08 10:17:38 -0600

    trustbar wip

---
+++ src/components/TrustBar.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const TrustBar = () => (
+	<div className="trustbar">
+		<h5>Code4rena has performed <strong>156</strong> audits.</h5>
+		<h5>Trusted by</h5>
+		<img src="opensea.svg" alt="OpenSea" />
+		<img src="ens.svg" alt="ENS" />
+		<img src="aave.svg" alt="Aave" />
+		<img src="pooltogether.svg" alt="PoolTogether" />
+		<img src="sushi.svg" alt="Sushi" />
+		<img src="slingshot.svg" alt="Slingshot" />
+		<img src="mechanism.png" alt="Mechanism" />
+		<img src="nascent.svg" alt="Nascent" />
+		<img src="1kx.svg" alt="1kx" />
+		<img src="framework.svg" alt="Framework" />
+		<img src="connext.svg" alt="Connext" />
+		<img src="foundation.svg" alt="Foundation" />
+		<img src="nounsdao.svg" alt="NounsDAO" />
+		<img src="olympusdao.svg" alt="OlympusDAO" />
+		<img src="tally.svg" alt="Tally Wallet" />
+		<img src="alchemix.svg" alt="Alchemix" />
+		<img src="fraxfinance.svg" alt="Frax Finance" />
+		<img src="unlock.svg" alt="Unlock" />
+		<img src="gravity-bridge.svg" alt="Gravity Bridge" />
+		<img src="yield.svg" alt="Yield" />
+		<img src="elasticswap.svg" alt="Elastic Swap" />
+		<img src="juicebox.svg" alt="JuiceBox" />
+		<img src="forgotten-runes.svg" alt="Forgotten Runes" />
+	</>
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default TrustBar;
