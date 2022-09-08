f4e7e856ae5482a9eb2ed84b3a176b340d21b728
Adam Avenir <adamavenir@hey.com>
2022-09-08 11:47:09 -0600

    add logos div

--- src/components/TrustBar.js
+++ src/components/TrustBar.js
@@ -3,78 +3,80 @@ import React from "react";
 const TrustBar = () => (
 	<div className="trustbar">
 		<h5>Trusted by</h5>
-		<a href="https://opensea.io">
-			<img src="/images/logos/opensea.svg" alt="OpenSea" />
-		</a>
-		<a href="https://ens.domains">	
-			<img src="/images/logos/ens.svg" alt="ENS" />
-		</a>
-		<a href="https://aave.com">
-			<img src="/images/logos/aave.svg" alt="Aave" />
-		</a>
-		<a href="https://pooltogether.com">
-			<img src="/images/logos/pooltogether.svg" alt="PoolTogether" />
-		</a>
-		<a href="https://sushi.com">
-			<img src="/images/logos/sushi.svg" alt="Sushi" />
-		</a>
-		<a href="https://slingshot.finance">
-			<img src="/images/logos/slingshot.svg" alt="Slingshot" />
-		</a>
-		<a href="https://mechanism.capital">
-			<img src="/images/logos/mechanism.png" alt="Mechanism" />
-		</a>
-		<a href="https://nascent.xyz">
-			<img src="/images/logos/nascent.svg" alt="Nascent" />
-		</a>
-		<a href="http://www.1kxfoundation.com/">
-			<img src="/images/logos/1kx.svg" alt="1kx" />
-		</a>
-		<a href="https://www.connext.network/">
-			<img src="/images/logos/connext.svg" alt="Connext" />
-		</a>
-		<a href="https://metacartel.xyz/">
-			<img src="/images/logos/metacartel.svg" alt="MetaCartel Venture DAO" />
-		</a>
-		<a href="https://foundation.app/">
-			<img src="/images/logos/foundation.svg" alt="Foundation" />
-		</a>
-		<a href="https://nouns.wtf/">
-			<img src="/images/logos/nounsdao.svg" alt="NounsDAO" />
-		</a>
-		<a href="https://www.olympusdao.finance/">
-			<img src="/images/logos/olympusdao.svg" alt="OlympusDAO" />
-		</a>
-		<a href="https://tally.cash">
-			<img src="/images/logos/tally.svg" alt="Tally Wallet" />
-		</a>
-		<a href="https://alchemix.fi">
-			<img src="/images/logos/alchemix.svg" alt="Alchemix" />
-		</a>
-		<a href="https://frax.finance/">
-			<img src="/images/logos/fraxfinance.svg" alt="Frax Finance" />
-		</a>
-		<a href="https://unlock-protocol.com/">
-			<img src="/images/logos/unlock.svg" alt="Unlock" />
-		</a>
-		<a href="https://maple.finance/">
-			<img src="/images/logos/maple.svg" alt="Maple Finance" />
-		</a>
-		<a href="https://www.gravitybridge.net/">
-			<img src="/images/logos/gravity-bridge.svg" alt="Gravity Bridge" />
-		</a>
-		<a href="https://www.yield.app/">
-			<img src="/images/logos/yield.svg" alt="Yield" />
-		</a>
-		<a href="https://elasticswap.org">
-			<img src="/images/logos/elasticswap.svg" alt="Elastic Swap" />
-		</a>
-		<a href="https://juicebox.money/">
-			<img src="/images/logos/juicebox.svg" alt="JuiceBox" />
-		</a>
-		<a href="https://www.forgottenrunes.com/">
-			<img src="/images/logos/forgotten-runes.svg" alt="Forgotten Runes" />
-		</a>
+		<div className="logos">
+			<a href="https://opensea.io">
+				<img src="/images/logos/opensea.svg" alt="OpenSea" />
+			</a>
+			<a href="https://ens.domains">	
+				<img src="/images/logos/ens.svg" alt="ENS" />
+			</a>
+			<a href="https://aave.com">
+				<img src="/images/logos/aave.svg" alt="Aave" />
+			</a>
+			<a href="https://pooltogether.com">
+				<img src="/images/logos/pooltogether.svg" alt="PoolTogether" />
+			</a>
+			<a href="https://sushi.com">
+				<img src="/images/logos/sushi.svg" alt="Sushi" />
+			</a>
+			<a href="https://slingshot.finance">
+				<img src="/images/logos/slingshot.svg" alt="Slingshot" />
+			</a>
+			<a href="https://mechanism.capital">
+				<img src="/images/logos/mechanism.png" alt="Mechanism" />
+			</a>
+			<a href="https://nascent.xyz">
+				<img src="/images/logos/nascent.svg" alt="Nascent" />
+			</a>
+			<a href="http://www.1kxfoundation.com/">
+				<img src="/images/logos/1kx.svg" alt="1kx" />
+			</a>
+			<a href="https://www.connext.network/">
+				<img src="/images/logos/connext.svg" alt="Connext" />
+			</a>
+			<a href="https://metacartel.xyz/">
+				<img src="/images/logos/metacartel.svg" alt="MetaCartel Venture DAO" />
+			</a>
+			<a href="https://foundation.app/">
+				<img src="/images/logos/foundation.svg" alt="Foundation" />
+			</a>
+			<a href="https://nouns.wtf/">
+				<img src="/images/logos/nounsdao.svg" alt="NounsDAO" />
+			</a>
+			<a href="https://www.olympusdao.finance/">
+				<img src="/images/logos/olympusdao.svg" alt="OlympusDAO" />
+			</a>
+			<a href="https://tally.cash">
+				<img src="/images/logos/tally.svg" alt="Tally Wallet" />
+			</a>
+			<a href="https://alchemix.fi">
+				<img src="/images/logos/alchemix.svg" alt="Alchemix" />
+			</a>
+			<a href="https://frax.finance/">
+				<img src="/images/logos/fraxfinance.svg" alt="Frax Finance" />
+			</a>
+			<a href="https://unlock-protocol.com/">
+				<img src="/images/logos/unlock.svg" alt="Unlock" />
+			</a>
+			<a href="https://maple.finance/">
+				<img src="/images/logos/maple.svg" alt="Maple Finance" />
+			</a>
+			<a href="https://www.gravitybridge.net/">
+				<img src="/images/logos/gravity-bridge.svg" alt="Gravity Bridge" />
+			</a>
+			<a href="https://www.yield.app/">
+				<img src="/images/logos/yield.svg" alt="Yield" />
+			</a>
+			<a href="https://elasticswap.org">
+				<img src="/images/logos/elasticswap.svg" alt="Elastic Swap" />
+			</a>
+			<a href="https://juicebox.money/">
+				<img src="/images/logos/juicebox.svg" alt="JuiceBox" />
+			</a>
+			<a href="https://www.forgottenrunes.com/">
+				<img src="/images/logos/forgotten-runes.svg" alt="Forgotten Runes" />
+			</a>
+		</div>
 	</div>
 )
 
