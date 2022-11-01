e6edb48016d08c67dbf5445e3e81ecab3e4f47ae
Captain Mango <captainmango@code423n4.com>
2022-11-01 16:30:27 -0700

    Hide repo and submit buttons for non-certified users if contest is open to only certified users

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -39,12 +39,6 @@ const getContestData = async () => {
                 does_not_equal: "Possible",
               },
             },
-            {
-              property: "Classified?",
-              checkbox: {
-                equals: false,
-              },
-            },
           ],
         },
       });
@@ -59,13 +53,30 @@ const getContestData = async () => {
         page.properties.Status.select.name !== "Lost deal" ||
         page.properties.Status.select.name !== "Possible" ||
         page.properties.Status.select.name ||
-        page.properties.ContestID.number ||
-        page.properties["Classified?"].checkbox === false
+        page.properties.ContestID.number
       ) {
-        return {
-          contestId: page.properties.ContestID.number || null,
-          status: page.properties.Status.select.name || null,
-        };
+        if (page.properties["Classified?"].checkbox === false) {
+          return {
+            contestId: page.properties.ContestID.number || null,
+            status: page.properties.Status.select.name || null,
+            codeAccess: "public",
+          };
+        } else if (
+          page.properties["Code access"].select &&
+          page.properties["Code access"].select.name.trim() === "Certified only"
+        ) {
+          return {
+            contestId: page.properties.ContestID.number || null,
+            status: page.properties.Status.select.name || null,
+            codeAccess: "certified",
+          };
+        } else {
+          return {
+            contestId: page.properties.ContestID.number || null,
+            status: page.properties.Status.select.name || null,
+            codeAccess: null,
+          };
+        }
       }
     });
     return statusObject;
@@ -245,17 +256,28 @@ exports.onCreateNode = async ({ node, getNode, actions }) => {
 exports.sourceNodes = async ({ actions, getNodes }) => {
   const { createNodeField } = actions;
   const nodes = await getNodes();
-  const result = await getContestData();
+  const contestStatusData = await getContestData();
 
   nodes.forEach((node, index) => {
     if (node.internal.type === `ContestsCsv`) {
-      const status = result.filter(
+      const dataForCurrentContest = contestStatusData.filter(
         (element) => element.contestId === node.contestid
       );
       createNodeField({
         node,
         name: `status`,
-        value: status.length > 0 ? status[0].status : undefined,
+        value:
+          dataForCurrentContest.length > 0
+            ? dataForCurrentContest[0].status
+            : undefined,
+      });
+      createNodeField({
+        node,
+        name: `codeAccess`,
+        value:
+          dataForCurrentContest.length > 0
+            ? dataForCurrentContest[0].codeAccess
+            : undefined,
       });
     }
   });
