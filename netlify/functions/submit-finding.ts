6333dba6a71e24fd07d8a9a0bdf19a6d63ef7252
Hannah Tuttle <hannahtuttle28@gmail.com>
2022-12-09 14:29:48 +0000

    updated submit findings function to handle mititgation submissions

--- netlify/functions/submit-finding.ts
+++ netlify/functions/submit-finding.ts
@@ -52,6 +52,7 @@ exports.handler = async (event) => {
   }
 
   const data = JSON.parse(event.body);
+  console.log(event.body);
   const {
     user,
     attributedTo,
@@ -63,9 +64,10 @@ exports.handler = async (event) => {
     sponsor,
     repo,
     address,
+    mitigationOf
   } = data;
   let emailAddresses: string[] = data.emailAddresses;
-
+console.log('mitigation of variable exitst', mitigationOf);
   await Moralis.start({
     serverUrl: moralisServerUrl,
     appId: moralisAppId,
@@ -88,10 +90,14 @@ exports.handler = async (event) => {
       }),
     };
   }
+  const isMitigationReport : boolean = mitigationOf ? true : false;
+  let mitigationLabels = ['mitigation-confirmed', `MR-${mitigationOf}`];
+  let mitigationTitle = `Mitigation Confirmed for ${mitigationOf}`;
 
   // ensure we have the data we need
   if (
-    emailAddresses.length == 0 ||
+    !isMitigationReport &&
+    (emailAddresses.length == 0 ||
     !user ||
     !risk ||
     !title ||
@@ -100,7 +106,7 @@ exports.handler = async (event) => {
     !contest ||
     !sponsor ||
     !attributedTo ||
-    !repo
+    !repo)
   ) {
     return {
       statusCode: 422,
@@ -248,25 +254,45 @@ exports.handler = async (event) => {
   }
 
   const owner = process.env.GITHUB_CONTEST_REPO_OWNER;
-  const riskCode = getRiskCodeFromGithubLabel(risk);
 
   try {
-    const markdownPath = `data/${attributedTo}-${riskCode}.md`;
-    const qaOrGasSubmissionBody = `See the markdown file with the details of this report [here](https://github.com/${owner}/${repo}/blob/main/${markdownPath}).`;
-    const isQaOrGasSubmission = Boolean(riskCode === "G" || riskCode === "Q");
-    if (isQaOrGasSubmission) {
-      const existingReport = await getMarkdownReportForUser(
-        octokit,
-        repo,
-        attributedTo,
-        riskCode as "Q" | "G"
-      );
-      if (existingReport) {
-        throw {
-          status: 400,
-          message: `It looks like you've already submitted a ${risk} report for this contest.`,
-        };
+    console.log('getting risk label causes problems??')
+    let isQaOrGasSubmission = false;
+    let qaOrGasSubmissionBody = '';
+    let markdownPath = '';
+    let riskCode = '';
+    if(!isMitigationReport){
+      const riskCode = getRiskCodeFromGithubLabel(risk);
+      markdownPath = `data/${attributedTo}-${riskCode}.md`;
+      qaOrGasSubmissionBody = `See the markdown file with the details of this report [here](https://github.com/${owner}/${repo}/blob/main/${markdownPath}).`;
+      isQaOrGasSubmission = Boolean(riskCode === "G" || riskCode === "Q");
+      if (isQaOrGasSubmission) {
+        const existingReport = await getMarkdownReportForUser(
+          octokit,
+          repo,
+          attributedTo,
+          riskCode as "Q" | "G"
+        );
+        if (existingReport) {
+          throw {
+            status: 400,
+            message: `It looks like you've already submitted a ${risk} report for this contest.`,
+          };
+        }
       }
+    }
+
+    //if it's a confirmed mitigation then no title will be provided
+    let isConfirmedMitigation = false;
+    if(isMitigationReport && (
+      !body || !title || !risk
+    )){
+      isConfirmedMitigation = true;
+    }else if(
+      isMitigationReport
+    ){
+      labels.push(`bug`)
+      labels.push(`MR-${mitigationOf}`)
     }
 
     const issueResult = await octokit.request(
@@ -274,9 +300,9 @@ exports.handler = async (event) => {
       {
         owner,
         repo,
-        title,
+        title : !isConfirmedMitigation? title : mitigationTitle,
         body: isQaOrGasSubmission ? qaOrGasSubmissionBody : body,
-        labels,
+        labels : !isConfirmedMitigation? labels : mitigationLabels,
       }
     );
 
@@ -293,7 +319,7 @@ exports.handler = async (event) => {
       issueId,
       issueUrl,
     };
-
+  
     const content = Buffer.from(JSON.stringify(fileData, null, 2)).toString(
       "base64"
     );
