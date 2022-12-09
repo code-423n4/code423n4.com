119ebf156adf8b9cef9d0fce2d56619661ccdbce
Joshua Short <joshua.short@gmail.com>
2022-12-09 03:13:34 -0800

    Move helpers to Netlify function

--- netlify/functions/leaderboard.ts
+++ netlify/functions/leaderboard.ts
@@ -1,3 +1,5 @@
+import { differenceInDays, getYear } from "date-fns";
+
 const getLeaderboardResults = async (
   handle: string,
   contestId: number,
@@ -45,6 +47,84 @@ const getLeaderboardResults = async (
 
   return result;
 };
+
+const withinLastNDays = (contestEnd, numDays) => {
+  return differenceInDays(new Date(), contestEnd) <= numDays;
+};
+
+const withinYear = (contestEnd, year) => {
+  return getYear(contestEnd) === year;
+};
+
+function filterFindingsByTimeFrame(findings, timeFrame) {
+  switch (timeFrame) {
+    case "Last 60 days":
+      return findings.filter((f) =>
+        withinLastNDays(new Date(f.contest.end_time), 60)
+      );
+    case "Last 90 days":
+      return findings.filter((f) =>
+        withinLastNDays(new Date(f.contest.end_time), 90)
+      );
+    case "2022":
+      return findings.filter((f) =>
+        withinYear(new Date(f.contest.end_time), 2022)
+      );
+    case "2021":
+      return findings.filter((f) =>
+        withinYear(new Date(f.contest.end_time), 2021)
+      );
+    default:
+      return findings;
+  }
+}
+
+function computeResults(findings) {
+  const results = {
+    lowRisk: 0,
+    medRisk: 0,
+    highRisk: 0,
+    soloMed: 0,
+    soloHigh: 0,
+    nonCrit: 0,
+    gasOptz: 0,
+    allFindings: 0,
+    awardTotal: 0,
+  };
+
+  findings.forEach((f) => {
+    results.allFindings += 1;
+    results.awardTotal += f.awardUSD ?? 0;
+
+    switch (f.risk.toLowerCase()) {
+      case "0":
+        results.nonCrit += 1;
+        break;
+      case "1":
+        results.lowRisk += 1;
+        break;
+      case "2":
+        results.medRisk += 1;
+        if (f.split === 1) {
+          results.soloMed += 1;
+        }
+        break;
+      case "3":
+        results.highRisk += 1;
+        if (f.split === 1) {
+          results.soloHigh += 1;
+        }
+        break;
+      case "g":
+        results.gasOptz += 1;
+        break;
+      default:
+        break;
+    }
+  });
+
+  return results;
+}
 
 exports.handler = async (event) => {
   // only allow GET
