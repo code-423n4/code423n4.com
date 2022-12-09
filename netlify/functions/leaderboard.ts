b8719d0d7ac8e090f29751bc642f746453d925b3
Joshua Short <joshua.short@gmail.com>
2022-12-09 02:36:36 -0800

    wip getLeaderboardResults

--- netlify/functions/leaderboard.ts
+++ netlify/functions/leaderboard.ts
@@ -1,3 +1,51 @@
+const getLeaderboardResults = async (
+  handle: string,
+  contestId: number,
+  contestRange: string,
+) => {
+  // @TODO: filter handles don't display on leaderboard
+  //   maybe also add links from files (so we'll be)
+
+  // read csv
+  // if using range, filter contest end_date
+  // if using handle, filter handle
+  // if using id, filter..
+
+  // const handleData = {
+  //   handle: p.handle,
+  //   image: p.image,
+  //   link: p.link,
+  //   members: p.members,
+  //   lowRisk: 0,
+  //   medRisk: 0,
+  //   soloMed: 0,
+  //   highRisk: 0,
+  //   soloHigh: 0,
+  //   nonCrit: 0,
+  //   gasOptz: 0,
+  //   allFindings: 0,
+  //   awardTotal: 0,
+  // };
+
+  let result = [{
+    handle: `${contestRange}-handle-1`,
+    image: "",
+    link: "",
+    members: [],
+    lowRisk: 1,
+    medRisk: 2,
+    soloMed: 0,
+    highRisk: 3,
+    soloHigh: 0,
+    nonCrit: 0,
+    gasOptz: 0,
+    allFindings: 6,
+    awardTotal: 10,
+  }];
+
+  return result;
+};
+
 exports.handler = async (event) => {
   // only allow GET
   if (event.httpMethod !== "GET") {
@@ -9,53 +57,18 @@ exports.handler = async (event) => {
   }
 
   try {
-    // range
-    const contestRange = event.queryStringParameters.range;
-
     // handle
     const contestHandle = event.queryStringParameters.handle;
 
     // contest
     const contestId = event.queryStringParameters.contest;
 
-    // @TODO: filter handles don't display on leaderboard
-    //   maybe also add links from files (so we'll be)
-
-    // const handleData = {
-    //   handle: p.handle,
-    //   image: p.image,
-    //   link: p.link,
-    //   members: p.members,
-    //   lowRisk: 0,
-    //   medRisk: 0,
-    //   soloMed: 0,
-    //   highRisk: 0,
-    //   soloHigh: 0,
-    //   nonCrit: 0,
-    //   gasOptz: 0,
-    //   allFindings: 0,
-    //   awardTotal: 0,
-    // };
-
-    let res = [{
-      handle: `${contestRange}-handle`,
-      image: "",
-      link: "",
-      members: [],
-      lowRisk: 1,
-      medRisk: 2,
-      soloMed: 0,
-      highRisk: 3,
-      soloHigh: 0,
-      nonCrit: 0,
-      gasOptz: 0,
-      allFindings: 6,
-      awardTotal: 10,
-    }];
+    // range
+    const contestRange = event.queryStringParameters.range;
 
     return {
       statusCode: 200,
-      body: JSON.stringify(res),
+      body: JSON.stringify(await getLeaderboardResults(contestHandle, contestId, contestRange)),
     };
   } catch (error) {
     return {
