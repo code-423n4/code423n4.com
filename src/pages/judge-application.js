aa6087dba40775148a27ce2cfc0ccba48573219b
Joshua Short <joshua.short@gmail.com>
2022-12-15 02:46:15 -0800

    cleanup judge page

--- src/pages/judge-application.js
+++ src/pages/judge-application.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-// import { StaticQuery, graphql } from "gatsby";
+import React, { useState } from "react";
 
 import Widgets from "../components/reporter/widgets/Widgets";
 import useUser from "../hooks/UserContext";
@@ -107,7 +106,6 @@ const JudgeApplication = () => {
     const url = `/.netlify/functions/apply-for-judge`;
     setStatus(FormStatus.Submitting);
     const sessionToken = user.attributes.sessionToken;
-    console.log(data);
     const response = await fetch(url, {
       method: "POST",
       headers: {
@@ -149,101 +147,101 @@ const JudgeApplication = () => {
   };
 
   return (
-          <ProtectedPage
-            bodyClass="judge-application"
-            pageTitle="Judge Application | Code4rena"
-          >
-            <div className="wrapper-main">
-              {(status === FormStatus.Unsubmitted ||
-                status === FormStatus.Submitting) && (
-                <>
-                  <h1 className="page-header">Judge Application</h1>
-                  <form>
-                    <Widgets
-                      fields={fields}
-                      onChange={handleChange}
-                      fieldState={state}
-                    />{" "}
-                    {status === FormStatus.Error && (
-                      <div
-                        className="error-message"
-                        role="alert"
-                        aria-atomic="true"
-                      >
-                        <p>
-                          An error occurred; please try again. If you continue
-                          to receive this error, let us know in{" "}
-                          <a href="https://discord.gg/code4rena">Discord</a>.
-                          All fields are required.
-                        </p>
-                      </div>
-                    )}
-                    <button
-                      className="button cta-button"
-                      type="button"
-                      onClick={handleSubmit}
-                    >
-                      {status === FormStatus.Submitting
-                        ? "Submitting..."
-                        : "Apply to be a Judge"}
-                    </button>
-                  </form>
-                </>
-              )}
-
-              {status === FormStatus.Submitted && (
-                <div className="thank-you">
-                  <h1>Thanks for applying!</h1>
-                  <p>
-                    <strong>Here's what happens next:</strong>
-                  </p>
-                  <ol>
-                    <li>
-                      Judge applications are reviewed by the C4 judge selection
-                      committee, which includes top leaderboard wardens and past
-                      judges. The committee will review your application and
-                      give you a "yes" or "not yet".
-                    </li>
-                    <li>
-                      The review process begins after the application window
-                      closes, and we expect it to take about a week, depending
-                      on the number of applications the committee receives.
-                    </li>
-                    <li>
-                      You'll be contacted via DM to let you know if your
-                      application has been successful this time around.
-                    </li>
-                    <li>
-                      If you're accepted as a judge, an organizer will onboard
-                      you and get you set up to judge your first contest!
-                    </li>
-                  </ol>
-                  <p>
-                    In the meantime, if you have questions, feel free to reach
-                    out to us in the C4 Discord, or have a closer look at the{" "}
-                    <a href="https://docs.code4rena.com/roles/judges/how-to-judge-a-contest">
-                      How to judge a contest
-                    </a>{" "}
-                    section in the Code4rena docs.
-                  </p>
-                </div>
-              )}
+    <ProtectedPage
+      bodyClass="judge-application"
+      pageTitle="Judge Application | Code4rena"
+    >
+      <div className="wrapper-main">
+        {(status === FormStatus.Unsubmitted ||
+          status === FormStatus.Submitting) && (
+          <>
+            <h1 className="page-header">Judge Application</h1>
+            <form>
+              <Widgets
+                fields={fields}
+                onChange={handleChange}
+                fieldState={state}
+              />{" "}
               {status === FormStatus.Error && (
-                <div className="centered-text">
-                  <h1>Whoops!</h1>
-                  <p>An error occurred while processing your application.</p>
-                  {errorMessage && <p>{errorMessage}</p>}
-                  <button
-                    className="button cta-button"
-                    type="button"
-                    onClick={() => setStatus(FormStatus.Unsubmitted)}
-                  >
-                    Try again
-                  </button>
+                <div
+                  className="error-message"
+                  role="alert"
+                  aria-atomic="true"
+                >
+                  <p>
+                    An error occurred; please try again. If you continue
+                    to receive this error, let us know in{" "}
+                    <a href="https://discord.gg/code4rena">Discord</a>.
+                    All fields are required.
+                  </p>
                 </div>
               )}
-            </div>
-          </ProtectedPage>
+              <button
+                className="button cta-button"
+                type="button"
+                onClick={handleSubmit}
+              >
+                {status === FormStatus.Submitting
+                  ? "Submitting..."
+                  : "Apply to be a Judge"}
+              </button>
+            </form>
+          </>
+        )}
+
+        {status === FormStatus.Submitted && (
+          <div className="thank-you">
+            <h1>Thanks for applying!</h1>
+            <p>
+              <strong>Here's what happens next:</strong>
+            </p>
+            <ol>
+              <li>
+                Judge applications are reviewed by the C4 judge selection
+                committee, which includes top leaderboard wardens and past
+                judges. The committee will review your application and
+                give you a "yes" or "not yet".
+              </li>
+              <li>
+                The review process begins after the application window
+                closes, and we expect it to take about a week, depending
+                on the number of applications the committee receives.
+              </li>
+              <li>
+                You'll be contacted via DM to let you know if your
+                application has been successful this time around.
+              </li>
+              <li>
+                If you're accepted as a judge, an organizer will onboard
+                you and get you set up to judge your first contest!
+              </li>
+            </ol>
+            <p>
+              In the meantime, if you have questions, feel free to reach
+              out to us in the C4 Discord, or have a closer look at the{" "}
+              <a href="https://docs.code4rena.com/roles/judges/how-to-judge-a-contest">
+                How to judge a contest
+              </a>{" "}
+              section in the Code4rena docs.
+            </p>
+          </div>
+        )}
+        {status === FormStatus.Error && (
+          <div className="centered-text">
+            <h1>Whoops!</h1>
+            <p>An error occurred while processing your application.</p>
+            {errorMessage && <p>{errorMessage}</p>}
+            <button
+              className="button cta-button"
+              type="button"
+              onClick={() => setStatus(FormStatus.Unsubmitted)}
+            >
+              Try again
+            </button>
+          </div>
+        )}
+      </div>
+    </ProtectedPage>
   );
 };
 
