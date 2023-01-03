5b955350dbd1f71584a923bd1e12bec68d4d6cfc
Hannah Tuttle <hannahtuttle28@gmail.com>
2023-01-03 20:33:50 +0000

    updated leaderboard to last 60 days to make it start tighter and then reordered to leaderboard option in ascending order

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -4,7 +4,7 @@ import DefaultLayout from "../templates/DefaultLayout";
 import LeaderboardTable from "../components/LeaderboardTable";
 
 export default function Leaderboard({ data }) {
-  const [timeFrame, setTimeFrame] = useState("All time");
+  const [timeFrame, setTimeFrame] = useState("Last 60 days");
   const [leaderboardResults, setLeaderboardResults] = useState([]);
 
   useEffect(() => {
@@ -30,12 +30,12 @@ export default function Leaderboard({ data }) {
   };
 
   const filterOptions = [
-    { value: "All time", label: "All time" },
-    { value: "2023", label: "2023" },
-    { value: "2022", label: "2022" },
-    { value: "2021", label: "2021" },
     { value: "Last 60 days", label: "Last 60 days" },
     { value: "Last 90 days", label: "Last 90 days" },
+    { value: "2021", label: "2021" },
+    { value: "2022", label: "2022" },
+    { value: "2023", label: "2023" },
+    { value: "All time", label: "All time" },
   ];
 
   return (
