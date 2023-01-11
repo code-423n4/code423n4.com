68ff4e61c37cf09a0cc9ba17dcbbd2c6f793a3e4
simon busch <simon.busch@live.fr>
2023-01-11 14:53:03 +0100

    revert

--- netlify/util/getContestsData.ts
+++ netlify/util/getContestsData.ts
@@ -1,15 +1,13 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
-const getApiContestData = async () => {  // only allow GET
+export const getApiContestData = async () => {
+  // only allow GET
   try {
     const res = await fetch(`${process.env.C4_API_URL}/api/v0/getContest`, {
       method: "GET",
     });
-    console.log("inside,", process.env.C4_API_URL);
     return await res.json();
   } catch (err) {
     console.log(err);
   }
 };
-
-module.exports = { getApiContestData };
