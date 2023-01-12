3f32cde7390c0fffefb65a9aaf6136e1394eca88
simon busch <simon.busch@live.fr>
2023-01-12 15:05:01 +0100

    [UPD] switch url ........

--- netlify/util/getContestsData.ts
+++ netlify/util/getContestsData.ts
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 const getApiContestData = async () => {
   try {
-    const res = await fetch(`http://localhost:8888/api/v0/getContest`, {
+    const res = await fetch(`${process.env.C4_API_URL}/api/v0/getContest`, {
       method: "POST",
       body: JSON.stringify({ token: process.env.C4_API_TOKEN }),
     });
