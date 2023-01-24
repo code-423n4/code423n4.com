8c51c81f3810c647996fc77a1ccc3709e009257f
simon busch <simon.busch@live.fr>
2023-01-24 15:39:13 +0100

    remove logs

--- gatsby-node.esm.js
+++ gatsby-node.esm.js
@@ -186,7 +186,6 @@ exports.sourceNodes = async ({
 }) => {
   const { createNode } = actions;
   const apiContestsData = await getApiContestData();
-  console.log(apiContestsData)
   apiContestsData.forEach((contest) => {
     createNode({
       ...contest,
