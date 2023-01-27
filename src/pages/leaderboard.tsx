93a35e371c2c0d96debba523ab6d255b19e0b49e
Hannah Tuttle <hannahtuttle28@gmail.com>
2023-01-27 14:28:15 +0000

    remove current year

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -33,7 +33,7 @@ export default function Leaderboard({ data }) {
     { value: "Last 90 days", label: "Last 90 days" },
     { value: "2021", label: "2021" },
     { value: "2022", label: "2022" },
-    { value: "Current Year", label: "Current Year" },
+    { value: "2023", label: "2023" },
     { value: "All time", label: "All time" },
   ];
 
