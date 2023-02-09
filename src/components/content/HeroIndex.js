299becfeb67a9afca48da6579d14070d6d1d3d3a
Burgertime <114885827+burgertime-c4@users.noreply.github.com>
2023-02-08 20:10:37 -0500

    Hero updates, not finished

--- src/components/content/HeroIndex.js
+++ src/components/content/HeroIndex.js
@@ -1,40 +1,60 @@
 import React from "react";
-import TrustBar from "../TrustBar";
 
 const primaryButtonText = "Get started";
 const secondaryButtonText = "View leaderboard";
 
 const HeroIndex = () => {
   return (
-    <div className="hero type__copy">
-      <h1 className="type__l-headline">Find bugs. Get paid.</h1>
-      <p>
-        Want to get paid for deploying your hacking abilities? You're in the
-        right place. Code4rena competitive audits reward you for finding valid
-        bugs, every time.
-      </p>
-      <div className="hero-buttons">
-        <a
-          href="https://code4rena.typeform.com/i-want-an-audit"
-          target="_blank"
-          rel="noreferrer"
-          aria-label={primaryButtonText + " (Opens in a new window)"}
-          className="button button-small"
-        >
-          {primaryButtonText}
-        </a>
-        <a
-          href="https://discord.gg/code4rena"
-          target="_blank"
-          rel="noreferrer"
-          aria-label={secondaryButtonText + " (Opens in a new window"}
-          className="button button-small cta-button lowercase secondary"
-        >
-          {secondaryButtonText}
-        </a>
+    <div className="hero hero__wardens type__copy grid__one-by-two">
+      <div>
+        <h1 className="type__hero-headline">
+          <div>Find bugs.</div>
+          <div>Get paid.</div>
+        </h1>
+        <p>
+          Want to get paid for deploying your hacking abilities? You're in the
+          right place. Code4rena competitive audits reward you for finding valid
+          bugs, every time.
+        </p>
+        <div className="hero__buttons">
+          <a
+            href="https://code4rena.typeform.com/i-want-an-audit"
+            target="_blank"
+            rel="noreferrer"
+            aria-label={primaryButtonText + " (Opens in a new window)"}
+            className="button button-small"
+          >
+            {primaryButtonText}
+          </a>
+          <a
+            href="https://discord.gg/code4rena"
+            target="_blank"
+            rel="noreferrer"
+            aria-label={secondaryButtonText + " (Opens in a new window"}
+            className="button button--secondary"
+          >
+            {secondaryButtonText}
+          </a>
+        </div>
       </div>
-
-      <TrustBar />
+      <div className="hero__right-side">
+        <div className="hero__statistic">
+          <p className="type__m-headline">$140m</p>
+          <p className="type__m-subheadline">Earned on-platform</p>
+        </div>
+        <div className="hero__statistic">
+          <p className="type__m-headline">$140m</p>
+          <p className="type__m-subheadline">Earned on-platform</p>
+        </div>
+        <div className="hero__statistic">
+          <p className="type__m-headline">$140m</p>
+          <p className="type__m-subheadline">Earned on-platform</p>
+        </div>
+        <div className="hero__statistic">
+          <p className="type__m-headline">$140m</p>
+          <p className="type__m-subheadline">Earned on-platform</p>
+        </div>
+      </div>
     </div>
   );
 };
