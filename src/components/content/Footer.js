c127f11bb47b271c086281da0a2a09fcd4fa3732
Burgertime <114885827+burgertime-c4@users.noreply.github.com>
2023-02-17 21:32:06 -0500

    basic footer

--- src/components/content/Footer.js
+++ src/components/content/Footer.js
@@ -2,16 +2,26 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <span>An open organization</span>
-      <a href="https://twitter.com/code4rena">Twitter</a>
-      <a href="https://discord.gg/code4rena">Discord</a>
-      <a href="https://github.com/code-423n4/">GitHub</a>
-      <span className="eth-address">
-        <a href="https://etherscan.io/address/0xC2BC2F890067C511215F9463A064221577A53E10">
-          0xC2bc2F890067C511215f9463a064221577a53E10
-        </a>
-      </span>
+    <footer className="footer limited-width">
+      <ul className="footer__items">
+        <li className="footer__item">An open organization</li>
+        <li className="footer__item">
+          <a href="https://twitter.com/code4rena">Twitter</a>
+        </li>
+        <li className="footer__item">
+          <a href="https://discord.gg/code4rena">Discord</a>
+        </li>
+        <li className="footer__item">
+          <a href="https://github.com/code-423n4/">GitHub</a>
+        </li>
+        <li className="footer__item">
+          <span className="eth-address">
+            <a href="https://etherscan.io/address/0xC2BC2F890067C511215F9463A064221577A53E10">
+              0xC2bc2F890067C511215f9463a064221577a53E10
+            </a>
+          </span>
+        </li>
+      </ul>
     </footer>
   );
 };
