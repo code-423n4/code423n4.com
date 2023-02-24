00f9180d35f0163aaab38cb1d6097dda4745ac01
Burgertime <114885827+burgertime-c4@users.noreply.github.com>
2023-02-24 11:27:46 -0500

    fixes for logged-in header styles

--- src/components/Login/UserDropdown.tsx
+++ src/components/Login/UserDropdown.tsx
@@ -18,9 +18,7 @@ export default function UserDropdown() {
         size="30px"
         round="30px"
       />
-      <span className={"user-dropdown__caret"} aria-hidden>
-        ▾
-      </span>
+      <span className={"user-dropdown__caret"} aria-hidden></span>
     </div>
   );
 
@@ -28,10 +26,9 @@ export default function UserDropdown() {
     <>
       <Dropdown
         triggerButton={avatar()}
-        wrapperClass={"user-dropdown__user-dropdown-wrapper"}
-        triggerButtonClass={"user-dropdown__user-dropdown"}
+        wrapperClass={"user-dropdown "}
+        triggerButtonClass={"user-dropdown__trigger"}
         openOnHover={true}
-        className={"user-dropdown__desktop"}
       >
         <div className={"user-dropdown__user-dropdown-list"}>
           <span className={"user-dropdown__user-info"}>
@@ -51,43 +48,6 @@ export default function UserDropdown() {
           </button>
         </div>
       </Dropdown>
-      <div className={"user-dropdown__mobile"}>
-        <span className={"user-dropdown__mobile-menu-item"}>
-          <span className={"user-dropdown__icon"}>
-            <Avatar
-              src={currentUser.image}
-              name={currentUser.username}
-              size="30px"
-              round="30px"
-            />
-          </span>
-          {currentUser.username}
-        </span>
-        <Link
-          to="/account"
-          className={clsx(
-            "user-dropdown__button",
-            "user-dropdown__mobile-menu-item"
-          )}
-        >
-          Manage Account
-        </Link>
-        <button
-          onClick={logUserOut}
-          className={clsx(
-            "user-dropdown__button",
-            "user-dropdown__mobile-menu-item"
-          )}
-        >
-          <img
-            src="/images/sign-out.svg"
-            alt="logout icon"
-            className={"user-dropdown__icon"}
-            style={{ transform: "rotateY(180deg)" }}
-          />
-          Logout
-        </button>
-      </div>
     </>
   );
 }
