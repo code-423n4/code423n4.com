b66d3f4fe8768ec14de797415781cf2469f98de8
Captain Mango <captainmango@code423n4.com>
2023-02-28 13:42:54 -0800

    Add solo findings back to the leaderboard

--- src/components/LeaderboardTableReduced.js
+++ src/components/LeaderboardTableReduced.js
@@ -79,10 +79,22 @@ const LeaderboardTableReduced = ({ results, isLoading, reduced }) => {
         className: "table__cell--number leaderboard__high",
       },
       {
+        Header: "(Solo)",
+        accessor: "soloHigh",
+        sortDescFirst: true,
+        className: "table__cell--number leaderboard__high-solo",
+      },
+      {
         Header: "Med",
         accessor: "medRisk",
         sortDescFirst: true,
         className: "table__cell--number leaderboard__med",
+      },
+      {
+        Header: "(Solo)",
+        accessor: "soloMed",
+        sortDescFirst: true,
+        className: "table__cell--number leaderboard__med-solo",
       },
       {
         Header: "Gas",
@@ -177,70 +189,6 @@ const LeaderboardTableReduced = ({ results, isLoading, reduced }) => {
           </ul>
         )}
       </div>
-
-      {/* <table {...getTableProps()} className="leaderboard-table table--zebra">
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  className={
-                    column.isSorted
-                      ? column.isSortedDesc
-                        ? "sort-desc"
-                        : "sort-asc"
-                      : "" + (column.isNumber ? " table__column--numbers" : "")
-                  }
-                >
-                  {column.render("Header")}
-                  <span className="sort-direction">
-                    {column.isSorted ? (column.isSortedDesc ? " ▼" : " ▲") : ""}
-                  </span>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        {rows.length > 0 && !isLoading ? (
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => (
-                    <td
-                      {...cell.getCellProps([
-                        {
-                          className: cell.column.className,
-                        },
-                      ])}
-                    >
-                      {cell.render("Cell")}
-                    </td>
-                  ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        ) : !isLoading ? (
-          <tbody>
-            <tr>
-              <td colSpan="9" className="center">
-                <h3 className="skeleton-loader">Fetching results...</h3>
-              </td>
-            </tr>
-          </tbody>
-        ) : (
-          <tbody>
-            <tr>
-              <td colSpan="9" className="center">
-                No results to show. Try changing filter criteria
-              </td>
-            </tr>
-          </tbody>
-        )}
-      </table> */}
     </>
   );
 };
