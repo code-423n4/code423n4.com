dc42b3051973db7a478d5fe0a1750c9b69241025
simon busch <simon.busch@live.fr>
2023-03-02 15:42:19 +0100

    remove logs on reporttitle

--- src/components/ReportTile.js
+++ src/components/ReportTile.js
@@ -8,26 +8,10 @@ import SponsorLink from "./SponsorLink";
 const ReportTile = ({ report }) => {
   const { slug, sponsor, contest, altUrl } = report;
   const { start_time, end_time } = contest;
-  if (!start_time || !end_time || !report.date || report.date === "") {
-    return <></>;
-  }
   const t = getDates(start_time, end_time);
-  if (t.startDay === "" || t.endDay === "") {
-    return <></>;
-  }
+
   const reportUrl = `/reports/${slug}`;
-  console.log("<<<<<>>>>>", sponsor)
-  console.log('data')
-  console.log(t.startDay, t.endDay, report.date)
-  console.log('data with new date')
-  console.log(new Date(t.startDay), new Date(t.endDay), new Date(report.date));
-  console.log('data with new date and format')
-  console.log(
-    format(new Date(t.startDay), "d MMM yyyy"),
-    format(new Date(t.endDay), "d MMM yyyy"),
-    format(new Date(report.date), "d MMM yyyy")
-  );
-  console.log("====================================")
+
   return (
     <div className={"report-tile"}>
       <div className="report-tile__content-wrapper">
