d05cca871ffce96d5b4f0ec60486a698632c964a
Captain Mango <captainmango@code423n4.com>
2023-03-16 13:46:47 -0700

    Add timer for bot registration page and show message instead of form when not live

--- src/pages/register/bot.tsx
+++ src/pages/register/bot.tsx
@@ -1,11 +1,18 @@
 import { graphql } from "gatsby";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import ProtectedPage from "../../components/ProtectedPage";
 import BotRegistrationForm from "../../components/BotRegistrationForm";
 import { WardenFieldOption } from "../../components/reporter/widgets/WardenField";
+import { isAfter, isBefore } from "date-fns";
+
+// @todo: replace with correct start and end times
+const START = new Date("2023-03-16T20:43:00.000Z");
+const END = new Date("2023-03-16T20:44:30.000Z");
 
 export default function TeamRegistration({ data }) {
+  const [isRegistrationOpen, setIsRegistrationOpen] = useState(false);
+
   const handles: Set<string> = new Set([
     ...data.handles.edges.map((h) => h.node.handle),
     ...data.bots.edges.map((b) => b.node.handle),
@@ -18,14 +25,36 @@ export default function TeamRegistration({ data }) {
     }
   });
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = Date.now();
+
+      if (isAfter(now, END)) {
+        setIsRegistrationOpen(false);
+        clearInterval(timer);
+      } else if (isBefore(now, END) && isAfter(now, START)) {
+        setIsRegistrationOpen(true);
+      } else {
+        setIsRegistrationOpen(false);
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  });
+
   return (
     <ProtectedPage
       pageTitle="Bot Application | Code4rena"
       message="You need to be a registered warden, currently connected via wallet to register a bot."
     >
       <div className="limited-width">
-        <h1>Register a Bot</h1>
-        <BotRegistrationForm handles={handles} wardens={wardens} />
+        {isRegistrationOpen ? (
+          <>
+            <h1>Register a Bot</h1>
+            <BotRegistrationForm handles={handles} wardens={wardens} />
+          </>
+        ) : (
+          <p>Bot registration is closed</p>
+        )}
       </div>
     </ProtectedPage>
   );
