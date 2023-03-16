bd10756292c0ed2d521d5b3c8df54ef78f435684
Captain Mango <captainmango@code423n4.com>
2023-03-16 14:23:26 -0700

    Update messaging for closed registration window

--- src/pages/register/bot.tsx
+++ src/pages/register/bot.tsx
@@ -4,14 +4,34 @@ import React, { useEffect, useState } from "react";
 import ProtectedPage from "../../components/ProtectedPage";
 import BotRegistrationForm from "../../components/BotRegistrationForm";
 import { WardenFieldOption } from "../../components/reporter/widgets/WardenField";
-import { isAfter, isBefore } from "date-fns";
+import { format, isAfter, isBefore } from "date-fns";
 
 // @todo: replace with correct start and end times
 const START = new Date("2023-03-16T20:43:00.000Z");
 const END = new Date("2023-03-16T20:44:30.000Z");
 
+enum Status {
+  soon,
+  open,
+  closed,
+}
+
+function getRegistrationWindowStatus(): Status {
+  const now = Date.now();
+  if (isAfter(now, END)) {
+    return Status.closed;
+  } else if (isBefore(now, END) && isAfter(now, START)) {
+    return Status.open;
+  } else {
+    return Status.soon;
+  }
+}
+
 export default function TeamRegistration({ data }) {
-  const [isRegistrationOpen, setIsRegistrationOpen] = useState(false);
+  const [
+    registrationWindowStatus,
+    setRegistrationWindowStatus,
+  ] = useState<Status>(getRegistrationWindowStatus());
 
   const handles: Set<string> = new Set([
     ...data.handles.edges.map((h) => h.node.handle),
@@ -27,16 +47,12 @@ export default function TeamRegistration({ data }) {
 
   useEffect(() => {
     const timer = setInterval(() => {
-      const now = Date.now();
+      const currentStatus = getRegistrationWindowStatus();
 
-      if (isAfter(now, END)) {
-        setIsRegistrationOpen(false);
+      if (currentStatus === Status.closed) {
         clearInterval(timer);
-      } else if (isBefore(now, END) && isAfter(now, START)) {
-        setIsRegistrationOpen(true);
-      } else {
-        setIsRegistrationOpen(false);
       }
+      setRegistrationWindowStatus(currentStatus);
     }, 1000);
     return () => clearInterval(timer);
   });
@@ -47,13 +63,38 @@ export default function TeamRegistration({ data }) {
       message="You need to be a registered warden, currently connected via wallet to register a bot."
     >
       <div className="limited-width">
-        {isRegistrationOpen ? (
+        {registrationWindowStatus === Status.open && (
           <>
             <h1>Register a Bot</h1>
             <BotRegistrationForm handles={handles} wardens={wardens} />
           </>
-        ) : (
-          <p>Bot registration is closed</p>
+        )}
+        {registrationWindowStatus === Status.closed && (
+          <>
+            <h1>Bot Registration is Closed</h1>
+            <p>
+              The first registration window for Bot Races has now closed. Keep
+              your eye on our{" "}
+              <a
+                href=""
+                rel="noreferrer"
+                aria-label="Discord announcements channel (Opens in a new window)"
+              >
+                announcements channel in Discord
+              </a>{" "}
+              to learn when the next one will be.
+            </p>
+          </>
+        )}
+        {registrationWindowStatus === Status.soon && (
+          <>
+            <h1>Bot Registration Coming Soon...</h1>
+            <p>
+              The first registration window for Bot Races will open for one hour
+              on {format(START, "d MMMM")} from {format(START, "h:mm")} to{" "}
+              {format(END, "h:mm a")}
+            </p>
+          </>
         )}
       </div>
     </ProtectedPage>
