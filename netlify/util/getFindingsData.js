a969450ca0910de24923ba6836a8d6983db2e05a
Nathan Vander Wilt <nate@calftrail.com>
2023-04-20 07:19:27 -0700

    fetch findings data via two split requests
    re. https://github.com/code-423n4/api.code4rena.com/pull/613 changes

--- netlify/util/getFindingsData.js
+++ netlify/util/getFindingsData.js
@@ -12,15 +12,22 @@ import fetch from "node-fetch";
  * @throws Various errors if data cannot be fetched or processed.
  */
 const getApiFindingsData = async () => {
-  const res = await fetch(`${process.env.C4_API_URL}/api/v0/getFindings`, {
-    method: "POST",
-    body: JSON.stringify({ token: process.env.C4_API_TOKEN }),
-  });
-  if (res.status !== 200) {
-    throw Error(`Bad response from API server: ${await res.text()}`);
+  const data = [];
+  for (const splitPart of ["first", "second"]) {
+    const res = await fetch(
+      `${process.env.C4_API_URL}/api/v0/getFindings?splitPart=${splitPart}`,
+      {
+        method: "POST",
+        body: JSON.stringify({ token: process.env.C4_API_TOKEN }),
+      }
+    );
+    if (res.status !== 200) {
+      throw Error(`Bad response from API server: ${await res.text()}`);
+    }
+    const partialData = await res.json();
+    data.push(...partialData);
   }
-
-  return await res.json();
+  return data;
 };
 
 export { getApiFindingsData };
