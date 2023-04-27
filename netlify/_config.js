49c995acab899e609a1e2b06174db73ae8b01a84
Nathan Vander Wilt <nate@calftrail.com>
2023-04-27 09:02:27 -0700

    align Gatsby with recent API-side config techniques
    see https://github.com/code-423n4/api.code4rena.com/pull/657

--- netlify/_config.js
+++ netlify/_config.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+const nextBotQualifier = require("_data/bot-race-qualifier.json");
+
 module.exports = {
   token: process.env.GITHUB_TOKEN,
   apiKey: process.env.MAILGUN_KEY,
@@ -10,9 +12,5 @@ module.exports = {
   moralisAppId: process.env.GATSBY_MORALIS_APP_ID,
   kickboxApiKey: process.env.KICKBOX_API_KEY,
   apiToken: process.env.C4_API_TOKEN,
-  // keep in sync with START and END in _data/bot-race-qualifier.json
-  nextBotQualifier: {
-    start: "2023-04-12T20:00:00.000Z",
-    end: "2023-04-12T21:00:00.000Z",
-  },
+  nextBotQualifier,
 };
